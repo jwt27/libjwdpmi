b577b63cd3c1139d5b01212aeb8cf284980d1c14
J.W. Jagersma <jwjagersma@gmail.com>
2017-02-26 07:58:49 +0100

    test

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <deque>
+#include <crt0.h>
+
+extern int _crt0_user_startup_flags = 0;
+
+int _crt0_startup_flags = 0
+    | _crt0_user_startup_flags
+    | _CRT0_FLAG_NMI_SIGNAL
+    | _CRT0_DISABLE_SBRK_ADDRESS_WRAP
+    | _CRT0_FLAG_NONMOVE_SBRK 
+    | _CRT0_FLAG_LOCK_MEMORY;
+    //| _CRT0_FLAG_NULLOK
+    //| _CRT0_FLAG_FILL_DEADBEEF;
+
+int main(std::deque<std::string>);
+
+int main(int argc, char** argv)
+{
+    _crt0_startup_flags &= ~_CRT0_FLAG_LOCK_MEMORY;
+    try { throw 0; } catch(...) { }     // Looks silly, but this speeds up subsequent exceptions.
+    
+    std::deque<std::string> args { };   // TODO: std::string_view when it's available
+    for (auto i = 0; i < argc; ++i)
+        args.emplace_back(argv[i]);
+    
+    try { return main(args); }
+    catch(const std::exception& e) { /* TODO */ }
+    catch(...) { /* TODO */ }
+}
