4c33935897d764b44f8f7addecff925a92978407
J.W. Jagersma <jwjagersma@gmail.com>
2017-03-12 20:42:50 +0100

    move memory_lock to detail namespace. remove code_lock.

--- include/jw/dpmi/lock.h
+++ include/jw/dpmi/lock.h
@@ -25,68 +25,63 @@ namespace jw
 {
     namespace dpmi
     {
-        class memory_lock
+        namespace detail
         {
-        public:
-            memory_lock(const memory_lock& c) = delete;
-            memory_lock(memory_lock&& m) noexcept : mem(m.mem) { m.locked = false; }
-
-            memory_lock& operator=(const memory_lock& c) = delete;
-            memory_lock& operator=(memory_lock&&) noexcept = default;
-
-            virtual ~memory_lock()
+            class memory_lock
             {
-                try { unlock(); }
-                catch (const std::exception& e)
+            public:
+                memory_lock(const memory_lock& c) = delete;
+                memory_lock(memory_lock&& m) noexcept : mem(m.mem) { m.locked = false; }
+
+                memory_lock& operator=(const memory_lock& c) = delete;
+                memory_lock& operator=(memory_lock&&) noexcept = default;
+
+                virtual ~memory_lock()
                 {
-                    std::cerr << "Caught exception in memory_lock destructor! \n"
-                        << "locked region: " << std::hex << mem.get_address() << " - " << (mem.get_address() + mem.get_size()) << "\n"
-                        << "error: " << e.what() << std::endl;
+                    try { unlock(); }
+                    catch (const std::exception& e)
+                    {
+                        std::cerr << "Caught exception in memory_lock destructor! \n"
+                            << "locked region: " << std::hex << mem.get_address() << " - " << (mem.get_address() + mem.get_size()) << "\n"
+                            << "error: " << e.what() << std::endl;
+                    }
+                    catch (...) { std::cerr << "Caught exception in memory_lock destructor!" << std::endl; }
                 }
-                catch (...) { std::cerr << "Caught exception in memory_lock destructor!" << std::endl; }
-            }
 
-        protected:
-            template<typename T>
-            memory_lock(selector segment, const T* ptr, std::size_t size_bytes) : mem(segment, ptr, size_bytes) { lock(); }
+            protected:
+                template<typename T>
+                memory_lock(selector segment, const T* ptr, std::size_t size_bytes) : mem(segment, ptr, size_bytes) { lock(); }
 
-            void lock()
-            {
-                if (locked) return;
-                mem.lock_memory();
-                locked = true;
-            }
+                void lock()
+                {
+                    if (locked) return;
+                    mem.lock_memory();
+                    locked = true;
+                }
 
-            void unlock()
-            {
-                if (!locked) return;
-                mem.unlock_memory();
-                locked = false;
-            }
+                void unlock()
+                {
+                    if (!locked) return;
+                    mem.unlock_memory();
+                    locked = false;
+                }
 
-            memory_descriptor mem;
-            bool locked { false };
-        };
-
-        // Locks the memory occupied by a function (in Code Segment)
-        // Finding the size of a function requires ugly hacks. It's easier to rely on _CRT0_FLAG_LOCK_MEMORY.
-        struct[[deprecated]] code_lock final : public memory_lock
-        {
-            template<typename T>
-            code_lock(const T* addr, std::size_t size_bytes) : memory_lock(get_cs(), addr, size_bytes) { }
-        };
+                memory_descriptor mem;
+                bool locked { false };
+            };
+        }
 
         // Locks the memory occupied by one or more objects (in Data Segment)
-        struct data_lock final : public memory_lock
+        struct data_lock final : public detail::memory_lock
         {
             template<typename T>
-            data_lock(const T* addr, std::size_t num_elements = 1) : memory_lock(get_ds(), addr, num_elements * sizeof(T)) { }
+            data_lock(const T* addr, std::size_t num_elements = 1) : memory_lock(get_ds(), reinterpret_cast<void*>(addr), num_elements * sizeof(T)) { }
             data_lock(const void* addr, std::size_t size_bytes) : memory_lock(get_ds(), addr, size_bytes) { }
         };
 
         // Locks the memory occupied by a class (in Data Segment) that derives from this.
         template <typename T>
-        struct class_lock : public memory_lock
+        struct class_lock : public detail::memory_lock
         {
             class_lock() : memory_lock(get_ds(), this, sizeof(T)) { }
             class_lock(class_lock&&) : class_lock() { }
