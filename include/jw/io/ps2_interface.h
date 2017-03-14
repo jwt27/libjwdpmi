a1f7b300ca498bc15893680de861f53231988e28
J.W. Jagersma <jwjagersma@gmail.com>
2017-03-14 14:28:05 +0100

    complete rewrite of ps2_interface command handling. now fully inline and no more dynamic allocation.

--- include/jw/io/ps2_interface.h
+++ include/jw/io/ps2_interface.h
@@ -23,19 +23,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <jw/io/ioport.h>
 #include <jw/thread/task.h>
 
+// TODO: clean this up
+
 namespace jw
 {
     namespace io
-    {
-            //TODO: mouse interface too.
-        class ps2_interface : public keyboard_interface, dpmi::class_lock<ps2_interface>
+    {     
+        class ps2_interface : public keyboard_interface, dpmi::class_lock<ps2_interface>    // TODO: mouse interface too.
         {
             static bool initialized;
 
-            //static std::deque<unsigned char> read_ps2_data();
-            //static void write_ps2_command(std::deque<unsigned char> bytes);
         public:
-
             virtual std::deque<detail::scancode> get_scancodes() override;
 
             scancode_set _scancode_set;
@@ -48,14 +46,14 @@ namespace jw
             {
                 if (get_scancode_set() != set3) return;
                 byte cmd = enable ? 0xFA : 0xF8;
-                command({ send_data, recv_ack }, { cmd });
+                command<send_data, recv_ack>({ cmd });
             }
 
             keyboard_leds current_led_state;
             virtual void set_leds(keyboard_leds state) override
             {
                 if (state == current_led_state) return;
-                command({ send_data, recv_ack, send_data, recv_ack }, { 0xED, state });
+                command<send_data, recv_ack, send_data, recv_ack>({ 0xED, state });
                 current_led_state = state;
             }
 
@@ -68,24 +66,67 @@ namespace jw
             virtual ~ps2_interface();
 
         private:
-            void write_to_controller(byte b);
-            void write_to_keyboard(byte b);
-            byte read_from_keyboard();
+            void write_to_controller(byte b)
+            {
+                thread::yield_while([this]() { return get_status().busy; });
+                command_port.write(b);
+            }
+
+            void write_to_keyboard(byte b)
+            {
+                thread::yield_while([this]() { return get_status().busy; });
+                data_port.write(b);
+            }
+
+            byte read_from_keyboard() //TODO: timeout check 
+            {
+                //do
+                //{
+                //    assert(!get_status().timeout_error);
+                //    assert(!get_status().parity_error);  // TODO: throw exceptions
+                //} while (!get_status().data_available);
+                thread::yield_while([this]() { return !get_status().data_available; });
+                auto b = data_port.read();
+                if (config.translate_scancodes) b = detail::scancode::undo_translation(b);
+                return b;
+            }
+
             void reset();
 
             enum cmd_sequence_element
             {
-                nop,
-                send_cmd,
-                send_data,
-                recv_any,
-                recv_discard_any,
-                recv_ack,
-                recv_data
+                nop,                // no operation
+                send_cmd,           // send to command port
+                send_data,          // send to data port
+                recv_any,           // receive any data
+                recv_discard_any,   // receive any data and discard it
+                recv_ack,           // receive data, expect ACK
+                recv_data,          // receive data, expect non-ACK
+                end_sequence        // end of sequence
             };
-            using cmd_sequence = std::deque<cmd_sequence_element>;
+            
+            template<cmd_sequence_element = nop> constexpr void do_ps2_command(const byte*&, byte&) { }
 
-            std::deque<byte> command(const cmd_sequence& sequence, const std::deque<byte>& bytes);
+            template<cmd_sequence_element cmd = nop, cmd_sequence_element next = end_sequence, cmd_sequence_element... etc> 
+            void ps2_command(const byte* in, byte& out)
+            {
+                if (cmd == end_sequence) return;
+                
+                do_ps2_command<cmd>(in, out);
+
+                if (cmd == send_cmd && next == send_data) 
+                    thread::yield_while([&]() { return !get_status().write_to_controller; });
+                ps2_command<next, etc...>(in, out);
+            }
+
+            template<cmd_sequence_element... cmd>
+            byte command(const std::initializer_list<byte>& data)
+            {
+                dpmi::irq_mask no_irq { 1 };
+                byte result;
+                ps2_command<cmd...>(data.begin(), result);
+                return result;
+            }
 
             struct[[gnu::packed]] controller_status
             {
@@ -103,7 +144,7 @@ namespace jw
             io::out_port<byte> command_port { 0x64 };
             io::io_port<byte> data_port { 0x60 };
 
-            auto get_status() { return status_port.read(); }
+            controller_status get_status() { return status_port.read(); }
 
             struct controller_configuration_data
             {
@@ -124,8 +165,8 @@ namespace jw
                 };
             } config, initial_config;
 
-            void read_config();
-            void write_config();
+            void read_config() { config.data = command<send_cmd, recv_data>({ 0x20 }); }
+            void write_config() { command<send_cmd, send_data>({ 0x60, config.data }); read_config(); }
 
             thread::task<void()> keyboard_update_thread;
 
@@ -136,18 +177,56 @@ namespace jw
             {
                 if (get_status().data_available)
                 {
-                    ack();
                     do
                     {
                         auto c = data_port.read();
-                        //std::cerr << "scancode: " << std::hex << (int)c << '\n';
+                        //std::clog << "scancode: " << std::hex << (int)c << '\n';
                         if (config.translate_scancodes) *detail::scancode::undo_translation_inserter(scancode_queue) = c;
                         else scancode_queue.push_back(c);
                     } while (get_status().data_available);
                     if (keyboard_update_thread) keyboard_update_thread->start();
+                    ack();
                 }
-                // TODO: command / ACK handling here
-            } };
+            }, dpmi::no_auto_eoi };
         };
+
+        template<> inline void ps2_interface::do_ps2_command<ps2_interface::send_cmd>(const byte*& in, byte&) { write_to_controller(*(in++)); }
+        template<> inline void ps2_interface::do_ps2_command<ps2_interface::send_data>(const byte*& in, byte&) { write_to_keyboard(*(in++)); }
+        template<> inline void ps2_interface::do_ps2_command<ps2_interface::recv_any>(const byte*&, byte& out) { out = read_from_keyboard(); }
+        template<> inline void ps2_interface::do_ps2_command<ps2_interface::recv_discard_any>(const byte*&, byte&) { read_from_keyboard(); }
+        template<> inline void ps2_interface::do_ps2_command<ps2_interface::recv_ack>(const byte*&, byte&) 
+        {
+            do
+            {
+                auto b = read_from_keyboard();
+                switch (b)
+                {
+                case ACK:
+                    return;
+                default:
+                    std::cerr << "PS/2 interface: expected ACK, got this: " << std::hex << (unsigned)b << std::endl;
+                case RESEND:
+                    throw std::runtime_error("Keyboard on fire.");
+                }
+            } while (true);
+        }
+
+        template<> inline void ps2_interface::do_ps2_command<ps2_interface::recv_data>(const byte*&, byte& out) 
+        {                         
+            do
+            {
+                out = read_from_keyboard();
+                switch (out)
+                {
+                case ACK:
+                    std::cerr << "PS/2 interface: unexpected ACK!" << std::endl;
+                    //break;
+                case RESEND:
+                    throw std::runtime_error("Keyboard on fire.");
+                default:
+                    return;
+                }
+            } while (true);
+        }
     }
 }
--- src/ps2_interface.cpp
+++ src/ps2_interface.cpp
@@ -44,16 +44,17 @@ namespace jw
         void ps2_interface::set_scancode_set(byte set)
         {
         #ifdef DOSBOX_BUG
-            command({ send_data, recv_ack, send_data, recv_ack, recv_ack }, { 0xF0, set }); // Dosbox-X sends two ACKs
+            command<send_data, recv_ack, send_data, recv_ack, recv_ack>({ 0xF0, set }); // Dosbox-X sends two ACKs
         #else
-            command({ send_data, recv_ack, send_data, recv_ack }, { 0xF0, set });
+            command<send_data, recv_ack, send_data, recv_ack>({ 0xF0, set });
         #endif
-
+            
+            
             _scancode_set = static_cast<scancode_set>(
         #ifdef DOSBOX_BUG
-                command({ send_data, recv_ack, send_data, recv_data, recv_ack }, { 0xF0, 0 })[0]);  // Dosbox-X sends ACK-data-ACK...
+                command<send_data, recv_ack, send_data, recv_data, recv_ack>({ 0xF0, 0 }));  // Dosbox-X sends ACK-data-ACK...
         #else
-                command({ send_data, recv_ack, send_data, recv_ack, recv_data }, { 0xF0, 0 })[0]);  // Should be ACK-ACK-data.
+                command<send_data, recv_ack, send_data, recv_ack, recv_data>({ 0xF0, 0 }));  // Should be ACK-ACK-data.
         #endif
         }
 
@@ -65,7 +66,7 @@ namespace jw
             config.keyboard_interrupt = true;
             write_config();
 
-            set_scancode_set(3);
+            set_scancode_set(2);    // TODO: set make/break mode for all keys in set3
             enable_typematic(true);
 
             irq_handler.enable();
@@ -88,113 +89,9 @@ namespace jw
         ps2_interface::~ps2_interface()
         {
             irq_handler.disable();
-            command({ send_data, recv_ack, recv_discard_any }, { 0xFF });  // reset keyboard
+            command<send_data, recv_ack, recv_discard_any>({ 0xFF });  // reset keyboard
             config = initial_config;
             write_config();                 // restore PS/2 configuration data
-        }
-
-        void ps2_interface::read_config() { config.data = command({ send_cmd, recv_data }, { 0x20 })[0]; }
-        void ps2_interface::write_config() { command({ send_cmd, send_data }, { 0x60, config.data }); read_config(); }
-
-        inline void ps2_interface::write_to_controller(byte b)
-        {
-            thread::yield_while([&]() { return get_status().busy; });
-            command_port.write(b);
-        }
-
-        inline void ps2_interface::write_to_keyboard(byte b)
-        {
-            thread::yield_while([&]() { return get_status().busy; });
-            data_port.write(b);
-        }
-
-        inline byte ps2_interface::read_from_keyboard() //TODO: timeout check 
-        {
-            //do
-            //{
-            //    assert(!get_status().timeout_error);
-            //    assert(!get_status().parity_error);  // TODO: throw exceptions
-            //} while (!get_status().data_available);
-            thread::yield_while([&]() { return !get_status().data_available; });
-            //while (!get_status().data_available) thread::yield();
-            auto b = data_port.read();
-            if (config.translate_scancodes) b = detail::scancode::undo_translation(b);
-            return b;
-        }
-
-        std::deque<byte> ps2_interface::command(const cmd_sequence& sequence, const std::deque<byte>& bytes)
-        {
-            byte data = 0;
-            std::deque<byte> response { };
-            auto cmd = bytes.begin();
-
-            dpmi::irq_mask disable_irq { 1 }; // TODO: don't disable IRQ, let IRQ deal with response
-            for (auto e = sequence.begin(); e != sequence.end(); ++e)
-            {
-                switch (*e)
-                {
-                case nop:
-                    break;
-                case send_cmd:
-                {
-                    write_to_controller(*(cmd++));
-                    auto next_e = e + 1;
-                    if (next_e != sequence.end() && (*next_e) == send_data)
-                        //scheduler::yield_while([&]() { return !get_status().write_to_controller; });
-                        while (!get_status().write_to_controller) thread::yield();
-                }
-                break;
-
-                case send_data:
-                    write_to_keyboard(*(cmd++));
-                    break;
-
-                case recv_any:
-                    response.push_back(read_from_keyboard());
-                    break;
-
-                case recv_discard_any:
-                    read_from_keyboard();
-                    break;
-
-                case recv_ack: //TODO: let ISR deal with recv
-                    do
-                    {
-                        data = read_from_keyboard();
-                        switch (data)
-                        {
-                        case ACK:
-                            break;
-                        case RESEND:
-                            throw std::runtime_error("Keyboard on fire.");
-                        default:
-                            std::cerr << "got scancode instead of ACK? " << std::hex << (unsigned)data << std::endl;
-                            scancode_queue.push_back(data); // TODO: translation
-                        }
-                    } while (data != ACK);
-                    break;
-
-                case recv_data:
-                    auto r_size = response.size();
-                    do
-                    {
-                        data = read_from_keyboard();
-                        switch (data)
-                        {
-                        case ACK:
-                            std::cerr << "--> unexpected ACK!" << std::endl;
-                            break;
-                        case RESEND:
-                            throw std::runtime_error("Keyboard on fire.");
-                        default:
-                            response.push_back(data);
-                        }
-                    } while (response.size() == r_size);
-                    break;
-                }
-            }
-
-            return response;
         }
     }
 }
