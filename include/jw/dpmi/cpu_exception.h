04caa2a319b889c11d4dcc91d9970a15c2d2c053
J.W. Jagersma <jwjagersma@gmail.com>
2017-03-15 18:07:27 +0100

    renamed cpu_exception class to cpu_exception_handlers, and moved to detail namespace in its own file.

--- include/jw/dpmi/cpu_exception.h
+++ include/jw/dpmi/cpu_exception.h
@@ -35,19 +35,19 @@ namespace jw
 {
     namespace dpmi
     {
-        struct [[gnu::packed]] old_exception_frame
+        struct[[gnu::packed]] old_exception_frame
         {
             far_ptr32 return_address; unsigned : 16;
             std::uint32_t error_code;
             far_ptr32 fault_address;
-            struct [[gnu::packed]] // DPMI 1.0 only
+            struct[[gnu::packed]] // DPMI 1.0 only
             {
                 bool host_exception : 1;
                 bool cannot_retry : 1;
                 bool redirect_elsewhere : 1;
                 unsigned : 13;
             } info_bits;
-            struct [[gnu::packed]]
+            struct[[gnu::packed]]
             {
                 bool carry : 1;
                 unsigned : 1;
@@ -74,14 +74,14 @@ namespace jw
             } flags;
             far_ptr32 stack; unsigned : 16;
         };
-        struct [[gnu::packed]] new_exception_frame : public old_exception_frame
+        struct[[gnu::packed]] new_exception_frame : public old_exception_frame
         {
             selector es; unsigned : 16;
             selector ds; unsigned : 16;
             selector fs; unsigned : 16;
             selector gs;
             std::uintptr_t linear_page_fault_address : 32;
-            struct [[gnu::packed]]
+            struct[[gnu::packed]]
             {
                 bool present : 1;
                 bool write_access : 1;
@@ -96,7 +96,7 @@ namespace jw
             } page_table_entry;
         };
 
-        struct [[gnu::packed]] raw_exception_frame
+        struct[[gnu::packed]] raw_exception_frame
         {
             cpu_registers reg;
             old_exception_frame frame_09;
@@ -106,108 +106,15 @@ namespace jw
         using exception_frame = old_exception_frame; // can be static_cast to new_exception_frame type
         using exception_handler_sig = bool(cpu_registers*, exception_frame*, bool);
         using exception_num = std::uint32_t;
+    }
+}
 
+#include <jw/dpmi/detail/cpu_exception.h>
 
-        struct cpu_exception
-        {
-            static far_ptr32 get_pm_handler(exception_num n)
-            {
-                try { return get_pm_exception_handler(n); }
-                catch (const dpmi_error& e)
-                {
-                    switch (e.code().value())
-                    {
-                    case dpmi_error_code::unsupported_function:
-                    case 0x0210:
-                        return get_exception_handler(n);
-                    default:
-                        throw;
-                    }
-                }
-            }
-
-            static far_ptr32 get_rm_handler(exception_num n) { return get_rm_exception_handler(n); }
-
-            static bool set_handler(exception_num n, far_ptr32 ptr, bool pm_only = false)
-            {
-                static bool is_new_type { true };
-
-                try { set_pm_exception_handler(n, ptr); }
-                catch (const dpmi_error& e)
-                {
-                    switch (e.code().value())
-                    {
-                    case dpmi_error_code::unsupported_function:
-                    case 0x0212:
-                        set_exception_handler(n, ptr);
-                        is_new_type = false;
-                        break;
-                    default:
-                        throw;
-                    }
-                }
-                if (!pm_only && is_new_type)
-                {
-                    try { set_rm_exception_handler(n, ptr); }
-                    catch (dpmi_error& e)
-                    {
-                        switch (e.code().value())
-                        {
-                        case dpmi_error_code::unsupported_function:
-                        case 0x0213:
-                            // too bad.
-                            break;
-                        default:
-                            throw e;
-                        }
-                    }
-                }
-                return is_new_type;
-            }
-
-        private:
-        #define CALL_INT31_GET(func_no, exc_no)                     \
-                dpmi_error_code error;                              \
-                bool c;                                             \
-                selector seg;                                       \
-                std::size_t offset;                                 \
-                asm("int 0x31;"                                     \
-                    : "=@ccc" (c)                                   \
-                    , "=a" (error)                                  \
-                    , "=c" (seg)                                    \
-                    , "=d" (offset)                                 \
-                    : "a" (func_no)                                 \
-                    , "b" (exc_no)                                  \
-                    : "cc");                                        \
-                if (c) throw dpmi_error(error, __FUNCTION__);       \
-                return far_ptr32(seg, offset);
-
-        #define CALL_INT31_SET(func_no, exc_no, handler_ptr)        \
-                dpmi_error_code error;                              \
-                bool c;                                             \
-                asm volatile(                                       \
-                    "int 0x31;"                                     \
-                    : "=@ccc" (c)                                   \
-                    , "=a" (error)                                  \
-                    : "a" (func_no)                                 \
-                    , "b" (exc_no)                                  \
-                    , "c" (handler_ptr.segment)                     \
-                    , "d" (handler_ptr.offset)                      \
-                    : "cc");                                        \
-                if (c) throw dpmi_error(error, __FUNCTION__);
-
-
-            static far_ptr32 get_exception_handler(exception_num n) { CALL_INT31_GET(0x0202, n); }      //DPMI 0.9 AX=0202
-            static far_ptr32 get_pm_exception_handler(exception_num n) { CALL_INT31_GET(0x0210, n); }   //DPMI 1.0 AX=0210
-            static far_ptr32 get_rm_exception_handler(exception_num n) { CALL_INT31_GET(0x0211, n); }   //DPMI 1.0 AX=0211
-
-            static void set_exception_handler(exception_num n, far_ptr32 handler) { CALL_INT31_SET(0x0203, n, handler); }       //DPMI 0.9 AX=0203
-            static void set_pm_exception_handler(exception_num n, far_ptr32 handler) { CALL_INT31_SET(0x0212, n, handler); }    //DPMI 1.0 AX=0212
-            static void set_rm_exception_handler(exception_num n, far_ptr32 handler) { CALL_INT31_SET(0x0213, n, handler); }    //DPMI 1.0 AX=0213
-
-        #undef CALL_INT31_SET
-        #undef CALL_INT31_GET
-        };
+namespace jw
+{
+    namespace dpmi
+    {
 
         class exception_handler : class_lock<exception_handler>
         {
@@ -239,11 +146,11 @@ namespace jw
 
                 if (!wrapper_list[e]) wrapper_list[e] = std::make_unique<std::deque<exception_handler*>>();
 
-                if (wrapper_list[e]->empty()) previous_handler = cpu_exception::get_pm_handler(e);
+                if (wrapper_list[e]->empty()) previous_handler = detail::cpu_exception_handlers::get_pm_handler(e);
                 else previous_handler = wrapper_list[e]->back()->get_ptr();
                 wrapper_list[e]->push_back(this);
 
-                new_type = cpu_exception::set_handler(e, get_ptr());
+                new_type = detail::cpu_exception_handlers::set_handler(e, get_ptr());
             }
 
             ~exception_handler();
---
+++ include/jw/dpmi/detail/cpu_exception.h
@@ -0,0 +1,128 @@
+/******************************* libjwdpmi **********************************
+Copyright (C) 2016-2017  J.W. Jagersma
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+namespace jw
+{
+    namespace dpmi
+    {
+        namespace detail
+        {
+            struct cpu_exception_handlers
+            {
+                static far_ptr32 get_pm_handler(exception_num n)
+                {
+                    try { return get_pm_exception_handler(n); }
+                    catch (const dpmi_error& e)
+                    {
+                        switch (e.code().value())
+                        {
+                        case dpmi_error_code::unsupported_function:
+                        case 0x0210:
+                            return get_exception_handler(n);
+                        default:
+                            throw;
+                        }
+                    }
+                }
+
+                static far_ptr32 get_rm_handler(exception_num n) { return get_rm_exception_handler(n); }
+
+                static bool set_handler(exception_num n, far_ptr32 ptr, bool pm_only = false)
+                {
+                    static bool is_new_type { true };
+
+                    try { set_pm_exception_handler(n, ptr); }
+                    catch (const dpmi_error& e)
+                    {
+                        switch (e.code().value())
+                        {
+                        case dpmi_error_code::unsupported_function:
+                        case 0x0212:
+                            set_exception_handler(n, ptr);
+                            is_new_type = false;
+                            break;
+                        default:
+                            throw;
+                        }
+                    }
+                    if (!pm_only && is_new_type)
+                    {
+                        try { set_rm_exception_handler(n, ptr); }
+                        catch (dpmi_error& e)
+                        {
+                            switch (e.code().value())
+                            {
+                            case dpmi_error_code::unsupported_function:
+                            case 0x0213:
+                                // too bad.
+                                break;
+                            default:
+                                throw e;
+                            }
+                        }
+                    }
+                    return is_new_type;
+                }
+
+            private:
+            #define CALL_INT31_GET(func_no, exc_no)                     \
+                dpmi_error_code error;                              \
+                bool c;                                             \
+                selector seg;                                       \
+                std::size_t offset;                                 \
+                asm("int 0x31;"                                     \
+                    : "=@ccc" (c)                                   \
+                    , "=a" (error)                                  \
+                    , "=c" (seg)                                    \
+                    , "=d" (offset)                                 \
+                    : "a" (func_no)                                 \
+                    , "b" (exc_no)                                  \
+                    : "cc");                                        \
+                if (c) throw dpmi_error(error, __FUNCTION__);       \
+                return far_ptr32(seg, offset);
+
+            #define CALL_INT31_SET(func_no, exc_no, handler_ptr)        \
+                dpmi_error_code error;                              \
+                bool c;                                             \
+                asm volatile(                                       \
+                    "int 0x31;"                                     \
+                    : "=@ccc" (c)                                   \
+                    , "=a" (error)                                  \
+                    : "a" (func_no)                                 \
+                    , "b" (exc_no)                                  \
+                    , "c" (handler_ptr.segment)                     \
+                    , "d" (handler_ptr.offset)                      \
+                    : "cc");                                        \
+                if (c) throw dpmi_error(error, __FUNCTION__);
+
+
+                static far_ptr32 get_exception_handler(exception_num n) { CALL_INT31_GET(0x0202, n); }      //DPMI 0.9 AX=0202
+                static far_ptr32 get_pm_exception_handler(exception_num n) { CALL_INT31_GET(0x0210, n); }   //DPMI 1.0 AX=0210
+                static far_ptr32 get_rm_exception_handler(exception_num n) { CALL_INT31_GET(0x0211, n); }   //DPMI 1.0 AX=0211
+
+                static void set_exception_handler(exception_num n, far_ptr32 handler) { CALL_INT31_SET(0x0203, n, handler); }       //DPMI 0.9 AX=0203
+                static void set_pm_exception_handler(exception_num n, far_ptr32 handler) { CALL_INT31_SET(0x0212, n, handler); }    //DPMI 1.0 AX=0212
+                static void set_rm_exception_handler(exception_num n, far_ptr32 handler) { CALL_INT31_SET(0x0213, n, handler); }    //DPMI 1.0 AX=0213
+
+            #undef CALL_INT31_SET
+            #undef CALL_INT31_GET
+            };
+        }
+    }
+}
