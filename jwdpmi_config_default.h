1d29fbbae578d5f617238c47f2dff91b37fbe5e6
J.W. Jagersma <jwjagersma@gmail.com>
2017-03-16 17:00:08 +0100

    disable non-call-exceptions for now since it's not working right.

--- jwdpmi_config_default.h
+++ jwdpmi_config_default.h
@@ -19,6 +19,6 @@ namespace jw
 
         const std::size_t thread_default_stack_size = 64_KB;
 
-        const bool enable_throwing_from_cpu_exceptions = true;
+        const bool enable_throwing_from_cpu_exceptions = false;
     }
 }
