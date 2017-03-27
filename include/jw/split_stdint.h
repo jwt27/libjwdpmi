304f91f3dca8daf7ff84595834b76b2c77f97039
J.W. Jagersma <jwjagersma@gmail.com>
2017-03-28 00:28:43 +0200

    changed split_uint to split_int, now works with signed types too.

--- include/jw/split_stdint.h
+++ include/jw/split_stdint.h
@@ -22,32 +22,35 @@ namespace jw
 {
     constexpr auto constexpr_min(auto a, auto b) { return std::min(a, b); }
 
-    template<std::size_t size>
-    union alignas(constexpr_min(size >> 3, 4ul)) [[gnu::packed]] split_uint
+    template<typename T, std::size_t size>
+    union alignas(constexpr_min(size >> 3, 4ul)) [[gnu::packed]] split_int
     {
         struct [[gnu::packed]]
         {
-            split_uint<(size >> 1)> lo;
-            split_uint<(size >> 1)> hi;
+            split_int<unsigned, (size >> 1)> lo;
+            split_int<T, (size >> 1)> hi;
         };
-        std::uint64_t value : size;
-        constexpr split_uint() noexcept { }
-        constexpr split_uint(auto v) noexcept : value(v) { };
+        std::conditional_t<std::is_signed<T>::value, std::int64_t, std::uint64_t> value : size;
+        constexpr split_int() noexcept { }
+        constexpr split_int(auto v) noexcept : value(v) { };
         constexpr operator auto() const noexcept { return value; }
     };
 
-    template<>
-    union alignas(1) [[gnu::packed]] split_uint<8>
+    template<typename T>
+    union alignas(1) [[gnu::packed]] split_int<T, 8>
     {
-        std::uint8_t value;
-        constexpr split_uint() noexcept : value(0) { }
-        constexpr split_uint(auto v) noexcept : value(v) { }
+        std::conditional_t<std::is_signed<T>::value, std::int8_t, std::uint8_t> value;
+        constexpr split_int() noexcept { }
+        constexpr split_int(auto v) noexcept : value(v) { }
         constexpr operator auto() const noexcept { return value; }
     };
 
-    using split_uint16_t = split_uint<16>;
-    using split_uint32_t = split_uint<32>;
-    using split_uint64_t = split_uint<64>;
+    using split_uint16_t = split_int<unsigned, 16>;
+    using split_uint32_t = split_int<unsigned, 32>;
+    using split_uint64_t = split_int<unsigned, 64>;
+    using split_int16_t = split_int<signed, 16>;
+    using split_int32_t = split_int<signed, 32>;
+    using split_int64_t = split_int<signed, 64>;
 
     static_assert(sizeof(split_uint64_t) == 8, "check sizeof jw::split_int");
     static_assert(alignof(split_uint64_t) == 4, "check alignof jw::split_int");
