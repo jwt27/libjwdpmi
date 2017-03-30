e4fd5c38e0f09aba7647174740d53333674bac19
J.W. Jagersma <jwjagersma@gmail.com>
2017-03-30 02:05:39 +0200

    chrono::pit_ticks doesn't need to be atomic.

--- include/jw/chrono/chrono.h
+++ include/jw/chrono/chrono.h
@@ -1,19 +1,5 @@
-/******************************* libjwdpmi **********************************
-Copyright (C) 2016-2017  J.W. Jagersma
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
+/* Copyright (C) 2017 J.W. Jagersma, see COPYING.txt for details */
 
 #pragma once
 #include <chrono>
@@ -55,7 +41,7 @@ namespace jw
             static double ns_per_pit_tick;
             static double ns_per_rtc_tick;
 
-            static std::atomic<std::uint64_t> pit_ticks;
+            static volatile std::uint64_t pit_ticks;
             static volatile std::uint_fast16_t rtc_ticks;
             
             static dpmi::irq_handler pit_irq;
