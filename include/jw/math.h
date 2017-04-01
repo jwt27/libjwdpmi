c6e6224ae90ea406bb71c0f1141e466110e40c3a
J.W. Jagersma <jwjagersma@gmail.com>
2017-04-01 21:08:38 +0200

    changed math.h functions to work with two different types.

--- include/jw/math.h
+++ include/jw/math.h
@@ -5,12 +5,12 @@
 
 namespace jw
 {
-    template<typename T> constexpr T remainder(T a, T b) { return a % b; }
+    template<typename T, typename U> constexpr T remainder(T a, U b) { return a % b; }
     template<> constexpr long double remainder(long double a, long double b) { return __builtin_remainderl(a, b); }
     template<> constexpr double remainder(double a, double b) { return __builtin_remainder(a, b); }
     template<> constexpr float remainder(float a, float b) { return __builtin_remainderf(a, b); }
 
-    template<typename T> constexpr T copysign(T a, T b) { return std::signbit(a) != std::signbit(b)? -a : a; }
+    template<typename T, typename U> constexpr T copysign(T a, U b) { return std::signbit(a) != std::signbit(b)? -a : a; }
     template<> constexpr long double copysign(long double a, long double b) { return __builtin_copysignl(a, b); }
     template<> constexpr double copysign(double a, double b) { return __builtin_copysign(a, b); }
     template<> constexpr float copysign(float a, float b) { return __builtin_copysignf(a, b); }
