50f6124b678d168b0475fa07557f51b2393d7ed7
J.W. Jagersma <jwjagersma@gmail.com>
2017-04-01 23:15:07 +0200

    new header: jw/video/pixel.h

---
+++ include/jw/video/pixel.h
@@ -0,0 +1,47 @@
+/* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
+/* Copyright (C) 2017 J.W. Jagersma, see COPYING.txt for details */
+
+#pragma once
+#include <jw/common.h>
+
+namespace jw
+{
+    namespace video
+    {
+        union [[gnu::packed]] text_attr
+        {
+            struct [[gnu::packed]]
+            {
+                unsigned foreground : 4;
+                unsigned background : 3;
+                bool blink : 1;
+            };
+            std::uint8_t raw_value;
+
+            constexpr text_attr() noexcept : text_attr(7, 0, false) { }
+            constexpr text_attr(byte fcol, byte bcol, bool _blink) noexcept : foreground(fcol), background(bcol), blink(_blink) { }
+        };
+
+        union alignas(2) [[gnu::packed]] text_char
+        {
+            struct [[gnu::packed]] char_with_attr
+            {
+                char character;
+                text_attr attr;
+
+                constexpr char_with_attr(char c, byte fcol, byte bcol, bool _blink) noexcept : character(c), attr(fcol, bcol, _blink) { }
+                constexpr char_with_attr(char c) noexcept : character(c), attr() { }
+            } value;
+            std::uint16_t raw_value;
+
+            constexpr text_char() noexcept : text_char(' ') { }
+            constexpr text_char(char c, byte fcol = 7, byte bcol = 0, bool _blink = false) noexcept : value(c, fcol, bcol, _blink) { }
+            constexpr explicit text_char(std::uint16_t v) noexcept : raw_value(v) { }
+            constexpr explicit operator std::uint16_t() const noexcept{ return raw_value; }
+            constexpr text_char& operator=(char c) noexcept { value.character = c; return *this; }
+            constexpr operator char() const noexcept{ return value.character; }
+        };
+
+        using pixel_pal8 = std::uint8_t;
+    }
+}
