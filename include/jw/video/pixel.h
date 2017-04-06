ac279a900776ac5d40e151545e8166c848747580
J.W. Jagersma <jwjagersma@gmail.com>
2017-04-06 05:37:12 +0200

    working on making pixel structures more generic and implicitly convertible to each other.

--- include/jw/video/pixel.h
+++ include/jw/video/pixel.h
@@ -43,39 +43,168 @@ namespace jw
         };
         static_assert(sizeof(text_char) == 2 && alignof(text_char) == 2, "text_char has incorrect size or alignment.");
 
+        struct alignas(0x10) bgra_ffff
+        {
+            using T = float;
+            T b { }, g { }, r { }, a { };
+
+            bgra_ffff() noexcept = default;
+            constexpr bgra_ffff(T cr, T cg, T cb, T ca) noexcept : b(cb), g(cg), r(cr), a(ca) { }
+
+            static constexpr T rx = 1.0f;
+            static constexpr T gx = 1.0f;
+            static constexpr T bx = 1.0f;
+            static constexpr T ax = 1.0f;
+            static constexpr bool has_alpha = true;
+        };
+
+        struct alignas(4) [[gnu::packed]] bgra_8888
+        {
+            using T = std::uint8_t;
+            T b, g, r, a;
+
+            bgra_8888() noexcept = default;
+            constexpr bgra_8888(T cr, T cg, T cb, T ca) noexcept : b(cb), g(cg), r(cr), a(ca) { }
+
+            static constexpr T rx = 255;
+            static constexpr T gx = 255;
+            static constexpr T bx = 255;
+            static constexpr T ax = 255;
+            static constexpr bool has_alpha = true;
+        };
+
+        struct [[gnu::packed]] bgra_8880
+        {
+            using T = std::uint8_t;
+            T b, g, r;
+
+            bgra_8880() noexcept = default;
+            constexpr bgra_8880(T cr, T cg, T cb, T) noexcept : b(cb), g(cg), r(cr) { }
+
+            static constexpr T rx = 255;
+            static constexpr T gx = 255;
+            static constexpr T bx = 255;
+            static constexpr T ax = 0;
+            static constexpr bool has_alpha = false;
+        };
+
+        struct alignas(2) [[gnu::packed]] bgra_5650
+        {
+            using T = unsigned;
+            T b : 5;
+            T g : 6;
+            T r : 5;
+
+            bgra_5650() noexcept = default;
+            constexpr bgra_5650(T cr, T cg, T cb, T) noexcept : b(cb), g(cg), r(cr) { }
+
+            static constexpr T rx = 31;
+            static constexpr T gx = 63;
+            static constexpr T bx = 31;
+            static constexpr T ax = 0;
+            static constexpr bool has_alpha = false;
+        };
+
+        struct alignas(2) [[gnu::packed]] bgra_5551
+        {
+            using T = unsigned;
+            T b : 5;
+            T g : 5;
+            T r : 5;
+            T a : 1;
+
+            bgra_5551() noexcept = default;
+            constexpr bgra_5551(T cr, T cg, T cb, T ca) noexcept : b(cb), g(cg), r(cr), a(ca) { }
+
+            static constexpr T rx = 31;
+            static constexpr T gx = 31;
+            static constexpr T bx = 31;
+            static constexpr T ax = 1;
+            static constexpr bool has_alpha = true;
+        };
+
+        template<typename P>
+        struct alignas(P) [[gnu::packed]] pixel : P
+        {
+            using T = typename P::T;
+
+            constexpr pixel() noexcept = default;
+            constexpr pixel(T cr, T cg, T cb, T ca) noexcept : P(cr, cg, cb, ca) { }
+            constexpr pixel(T cr, T cg, T cb) noexcept : P(cr, cg, cb, P::ax) { }
+            constexpr pixel(const pixel& p) noexcept = default;
+            constexpr pixel(pixel&& p) noexcept = default;
+
+            template <typename U> constexpr pixel<U> cast(std::true_type) const noexcept
+            { 
+                return pixel<U> 
+                { 
+                    static_cast<typename U::T>(P::r * max<U, P>(U::rx) / max<U, P>(P::rx)),
+                    static_cast<typename U::T>(P::g * max<U, P>(U::gx) / max<U, P>(P::gx)),
+                    static_cast<typename U::T>(P::b * max<U, P>(U::bx) / max<U, P>(P::bx)),
+                    static_cast<typename U::T>(P::a * max<U, P>(U::ax) / max<U, P>(P::ax))
+                };
+            };
+
+            template <typename U> constexpr pixel<U> cast(std::false_type) const noexcept
+            { 
+                return pixel<U> 
+                { 
+                    static_cast<typename U::T>(P::r * max<U, P>(U::rx) / max<U, P>(P::rx)),
+                    static_cast<typename U::T>(P::g * max<U, P>(U::gx) / max<U, P>(P::gx)),
+                    static_cast<typename U::T>(P::b * max<U, P>(U::bx) / max<U, P>(P::bx))
+                };
+            };
+
+            template <typename U> constexpr pixel<U> cast() const noexcept { return cast<U>(std::is_void<std::conditional_t<P::has_alpha, void, bool>> { }); }
+            template <typename U> constexpr operator pixel<U>() const noexcept { return cast<U>(); }
+
+            template <typename U> constexpr pixel& operator=(const pixel<U>& other) noexcept { return blend(other); }
+            constexpr pixel& operator=(const pixel& other) noexcept { return blend(other); }
+            template <typename U> constexpr pixel& operator=(pixel<U>&& other) noexcept { return *this = static_cast<pixel&&>(other); }
+            constexpr pixel& operator=(pixel&& o) noexcept = default;// { new(this) pixel { std::move(o) }; }
+
+            template <typename U, typename V, std::enable_if_t<(std::is_integral<typename U::T>::value && std::is_integral<typename V::T>::value), bool> = { }> 
+            static constexpr std::uint32_t max(auto max) noexcept { return max + 1; }
+            template <typename U, typename V, std::enable_if_t<(std::is_floating_point<typename U::T>::value || std::is_floating_point<typename V::T>::value), bool> = { }> 
+            static constexpr float max(auto max) noexcept { return max; }
+
+            template <typename U, std::enable_if_t<(std::is_floating_point<typename U::T>::value || std::is_floating_point<typename P::T>::value), bool> = { }>
+            constexpr pixel& blend(const pixel<U>& other)
+            {
+                pixel<std::conditional_t<std::is_floating_point<U>::value, U, P>> src { other };
+                pixel<std::conditional_t<std::is_floating_point<P>::value, P, U>> dst { *this };
+                //auto ta = src.a + dst.a * (1 - src.a);
+                //dst.b = (src.b * src.a + dst.b * dst.a * (1 - src.a)) / ta;
+                //dst.g = (src.g * src.a + dst.g * dst.a * (1 - src.a)) / ta;
+                //dst.r = (src.r * src.a + dst.r * dst.a * (1 - src.a)) / ta;
+                //dst.a = ta;
+                return *this = std::move(src);
+            }
+
+            template <typename U, std::enable_if_t<(std::is_integral<typename U::T>::value && std::is_integral<typename P::T>::value), bool> = { }> 
+            constexpr pixel& blend(const pixel<U>& o)
+            {
+                return *this = std::move(o);
+            }
+        };
+
         struct [[gnu::packed]] px8
         {
             byte value { };
+
             constexpr px8() noexcept = default;
             constexpr px8(byte v) : value(v) { }
             constexpr px8& operator=(byte p) { value = (p == 0 ? value : p); return *this; }
             constexpr px8& operator=(const px8& p) { value = (p.value == 0 ? value : p.value); return *this; }
             constexpr operator byte() { return value; }
-        };
-        static_assert(sizeof(px8) == 1 , "px8 has incorrect size.");
 
-        struct alignas(2) [[gnu::packed]] px15
-        {
-            unsigned b : 5;
-            unsigned g : 5;
-            unsigned r : 5;
+            template<typename T> constexpr auto cast(const auto& pal) { const auto& p = pal[value]; return T { p.r, p.g, p.b, p.a }; }
         };
 
-        struct alignas(2) [[gnu::packed]] px16
-        {
-            unsigned b : 5;
-            unsigned g : 6;
-            unsigned r : 5;
-        };
-
-        struct [[gnu::packed]] px24
-        {
-            std::uint8_t b, g, r;
-        };
-
-        struct alignas(4) [[gnu::packed]] px32
-        {
-            std::uint8_t b, g, r, a;
-        };
+        using pxf = pixel<bgra_ffff>;
+        using px32 = pixel<bgra_8888>;
+        using px24 = pixel<bgra_8880>;
+        using px16 = pixel<bgra_5650>;
+        using px15 = pixel<bgra_5551>;
     }
 }
