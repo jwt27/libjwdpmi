a49c500e2511112d0cb3cb2d1669ff92a57c0b1c
J.W. Jagersma <jwjagersma@gmail.com>
2017-04-09 22:51:49 +0200

    added another overload for pixel::cast() where the cast-to type has alpha but cast-from doesn't.

--- include/jw/video/pixel.h
+++ include/jw/video/pixel.h
@@ -282,6 +282,32 @@ namespace jw
                 }
             };
 
+            template <typename V, typename U, std::enable_if_t<!V::has_alpha && U::has_alpha, bool> = { }>
+            constexpr pixel<U> cast() const noexcept
+            { 
+                using max_T = decltype(max<U>(U::ax));
+                if (std::is_floating_point<max_T>::value)
+                {
+                    using vec = vector<max_T>;
+                    vec maxp { max<U>(V::rx), max<U>(V::gx), max<U>(V::bx), 1 };
+                    vec maxu { max<U>(U::rx), max<U>(U::gx), max<U>(U::bx), max<U>(V::ax) };
+                    vec src { this->r, this->g, this->b, 1 };
+                    src.v *= maxu.v;
+                    src.v /= maxp.v;
+                    return pixel<U> { }.assign_round<U>(src);
+                }
+                else
+                {
+                    using UT = typename U::T;
+                    pixel<U> result { };
+                    result.b = static_cast<UT>(this->b * max<U>(U::bx) / max<U>(V::bx));
+                    result.g = static_cast<UT>(this->g * max<U>(U::gx) / max<U>(V::gx));
+                    result.r = static_cast<UT>(this->r * max<U>(U::rx) / max<U>(V::rx));
+                    result.a = U::ax;
+                    return result;
+                }
+            };
+
             template <typename U> constexpr pixel<U> cast_to() const noexcept { return cast<P, U>(); }
             template <typename U> constexpr operator pixel<U>() const noexcept { return cast_to<U>(); }
 
