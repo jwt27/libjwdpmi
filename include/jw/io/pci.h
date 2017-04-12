5a3a5c186f9d3aa4b229542088e19d14c891d8e2
J.W. Jagersma <jwjagersma@gmail.com>
2017-04-13 00:52:07 +0200

    implemented pci_device constructor and destructor.

--- include/jw/io/pci.h
+++ include/jw/io/pci.h
@@ -2,6 +2,8 @@
 /* Copyright (C) 2017 J.W. Jagersma, see COPYING.txt for details */
 
 #pragma once
+#include <initializer_list>
+#include <unordered_map>
 #include <jw/dpmi/realmode.h>
 #include <jw/common.h>
 
@@ -11,21 +13,19 @@ namespace jw
     {
         struct pci_device
         {
-        protected:
-            pci_device(auto vendor, auto devices)
-            {
-                dpmi::realmode_registers reg { };
-                reg.ax = 0xb101;
-                reg.edi = 0;
-                reg.call_int(0x1A);
-                if (reg.edx != 0x20494350) throw std::runtime_error { "PCI not supported." };
+            struct error : public std::runtime_error { using runtime_error::runtime_error; };
+            struct unsupported_function : public error { using error::error; };
+            struct bad_register : public error { using error::error; };
+            struct device_not_found : public error { using error::error; };
 
-            }
+        protected:
+            pci_device(std::uint16_t vendor, std::initializer_list<std::uint16_t> devices, std::uint8_t function = 0xff);
+            virtual ~pci_device();
 
             template<typename T, std::uint8_t reg>
             struct pci_register
             {
-                constexpr pci_register(pci_device* device) : dev(device) { }
+                constexpr pci_register(pci_device* device) : dev(*device) { }
 
                 auto read()
                 {
@@ -38,11 +38,20 @@ namespace jw
                 }
 
             private:
-                pci_device* dev { };
+                pci_device& dev { };
             };
 
+        private:
             std::uint16_t vendor_id;
             std::uint16_t device_id;
+            std::uint8_t bus;
+            std::uint8_t bus_device;
+            std::uint16_t index;
+            std::uint8_t function_id;
+
+            // map indexed by: vendor->device->index
+            using map_type = std::unordered_map<std::uint16_t, std::unordered_map<std::uint16_t, std::unordered_map<std::uint16_t, pci_device*>>>;
+            static map_type* device_map;
         };
     }
 }
--- src/pci.cpp
+++ src/pci.cpp
@@ -7,6 +7,49 @@ namespace jw
 {
     namespace io
     {
+        pci_device::map_type* pci_device::device_map;
 
+        pci_device::pci_device(std::uint16_t vendor, std::initializer_list<std::uint16_t> devices, std::uint8_t function)
+        {
+            dpmi::realmode_registers reg { };
+            reg.ax = 0xb101;
+            reg.edi = 0;
+            reg.call_int(0x1a);
+            if (reg.flags.carry || reg.ah != 0 || reg.edx != 0x20494350) throw unsupported_function { "PCI not supported." };
+            if (device_map == nullptr) device_map = new map_type { };
+
+            vendor_id = vendor;
+            for (auto device : devices)
+            {
+                device_id = device;
+                for (auto i = 0;; ++i)
+                {
+                    if ((*device_map)[vendor_id][device_id].count(i)) continue;
+                    reg = { };
+                    reg.ax = 0xb102;
+                    reg.cx = device_id;
+                    reg.dx = vendor_id;
+                    reg.si = i;
+                    reg.call_int(0x1a);
+                    if (reg.ah == 0x81) throw unsupported_function { "Function \"find PCI device\" not supported." };
+                    if (reg.ah == 0x86) throw device_not_found { "PCI Device not found." };
+                    if (reg.ah == 0x83) throw device_not_found { "Bad vendor ID." };
+                    if (reg.flags.carry) throw error { "Unknown PCI error." };
+                    if (function != 0xff && (reg.bl & 0b111) != function) continue;
+                    bus = reg.bh;
+                    bus_device = reg.bl >> 3;
+                    (*device_map)[vendor_id][device_id][index] = this;
+                    return;
+                }
+            }
+        }
+
+        pci_device::~pci_device()
+        {
+            device_map[vendor_id][device_id].erase(index);
+            if ((*device_map)[vendor_id][device_id].empty()) device_map[vendor_id].erase(device_id);
+            if ((*device_map)[vendor_id].empty()) device_map->erase(vendor_id);
+            if (device_map->empty()) delete device_map;
+        }
     }
 }
