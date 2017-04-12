cc1d91ad9bdabbfe7f98eb93d87bed4a4482b2b6
J.W. Jagersma <jwjagersma@gmail.com>
2017-04-12 23:38:47 +0200

    new file: pci.cpp

---
+++ src/pci.cpp
@@ -0,0 +1,12 @@
+/* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
+/* Copyright (C) 2017 J.W. Jagersma, see COPYING.txt for details */
+
+#include <jw/io/pci.h>
+
+namespace jw
+{
+    namespace io
+    {
+
+    }
+}
