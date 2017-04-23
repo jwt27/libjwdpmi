e2002d7bb0383d2e02d88d393da89f44082e57f8
J.W. Jagersma <jwjagersma@gmail.com>
2017-04-23 22:45:24 +0200

    changed const variables in config file to constexpr.

--- jwdpmi_config_default.h
+++ jwdpmi_config_default.h
@@ -8,36 +8,36 @@ namespace jw
     {
         // Additional startup flags for the djgpp runtime library.
         // See http://www.delorie.com/djgpp/doc/libc/libc_124.html
-        const int user_crt0_startup_flags = 0;
+        constexpr int user_crt0_startup_flags = 0;
 
         // Initial stack size for IRQ handlers.
-        const std::size_t interrupt_initial_stack_size = 1_MB;
+        constexpr std::size_t interrupt_initial_stack_size = 1_MB;
 
         // Minimum stack size for IRQ handlers. Tries to resize when the stack size drops below this amount.
-        const std::size_t interrupt_minimum_stack_size = 64_KB;
+        constexpr std::size_t interrupt_minimum_stack_size = 64_KB;
 
         // Total memory allocated to store fpu contexts.
-        const std::size_t interrupt_fpu_context_pool = 32_KB;
+        constexpr std::size_t interrupt_fpu_context_pool = 32_KB;
 
         // Initial memory pool for operator new() in interrupt context.
-        const std::size_t interrupt_initial_memory_pool = 1_MB;
+        constexpr std::size_t interrupt_initial_memory_pool = 1_MB;
 
         // Total stack size for exception handlers. Remote debugging requires a lot of stack space.
-        const std::size_t exception_stack_size = 1_MB;
+        constexpr std::size_t exception_stack_size = 1_MB;
 
         // Default stack size for threads.
-        const std::size_t thread_default_stack_size = 64_KB;
+        constexpr std::size_t thread_default_stack_size = 64_KB;
 
         // Set up cpu exception handlers to throw C++ exceptions instead.
-        const bool enable_throwing_from_cpu_exceptions = true;
+        constexpr bool enable_throwing_from_cpu_exceptions = true;
 
         // Enable interrupts while the gdb interface is active
-        const bool enable_gdb_interrupts = false;
+        constexpr bool enable_gdb_interrupts = false;
 
         // Enable debug messages from the gdb interface
-        const bool enable_gdb_debug_messages = false;
+        constexpr bool enable_gdb_debug_messages = false;
 
         // Enable this to work around buggy keyboard code in dosbox.
-        const bool dosbox = false;
+        constexpr bool dosbox = false;
     }
 }
