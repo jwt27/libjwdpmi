2d5392cf517783f446f5f5058fd5690e5269fe58
J.W. Jagersma <jwjagersma@gmail.com>
2017-05-04 23:27:50 +0200

    fixed implicit-fallthrough warning on some case labels.

--- include/jw/io/ps2_interface.h
+++ include/jw/io/ps2_interface.h
@@ -1,19 +1,6 @@
-/******************************* libjwdpmi **********************************
-Copyright (C) 2016-2017  J.W. Jagersma
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
+/* Copyright (C) 2017 J.W. Jagersma, see COPYING.txt for details */
+/* Copyright (C) 2016 J.W. Jagersma, see COPYING.txt for details */
 
 #pragma once
 #include <atomic>
@@ -220,6 +207,7 @@ namespace jw
                     return;
                 default:
                     std::cerr << "PS/2 interface: expected ACK, got this: " << std::hex << (unsigned)b << std::endl;
+                    [[fallthrough]];
                 case RESEND:
                     throw std::runtime_error("Keyboard on fire.");
                 }
@@ -227,7 +215,7 @@ namespace jw
         }
 
         template<> inline void ps2_interface::do_ps2_command<ps2_interface::recv_data>(const byte*&, byte& out) 
-        {                         
+        {
             do
             {
                 out = read_from_keyboard();
@@ -236,6 +224,7 @@ namespace jw
                 case ACK:
                     std::cerr << "PS/2 interface: unexpected ACK!" << std::endl;
                     //break;
+                    [[fallthrough]];
                 case RESEND:
                     throw std::runtime_error("Keyboard on fire.");
                 default:
