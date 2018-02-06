a5c5ab809445c99ea58329869299b8ce46ae9c7a
J.W. Jagersma <jwjagersma@gmail.com>
2018-02-06 11:43:58 +0100

    bump copyright year

--- src/fpu.cpp
+++ src/fpu.cpp
@@ -1,4 +1,5 @@
 /* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
+/* Copyright (C) 2018 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2017 J.W. Jagersma, see COPYING.txt for details */
 
 #include <jw/dpmi/fpu.h>
