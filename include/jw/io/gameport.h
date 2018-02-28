a941207c6cab0d1a664801d929a499586d53d28d
J.W. Jagersma <jwjagersma@gmail.com>
2018-02-28 16:55:24 +0100

    implemented gameport::poll().

--- include/jw/io/gameport.h
+++ include/jw/io/gameport.h
@@ -6,6 +6,7 @@
 #include <jw/chrono/chrono.h>
 #include <jw/dpmi/irq.h>
 #include <jw/thread/task.h>
+#include <limits>
 
 namespace jw::io
 {
@@ -32,14 +33,14 @@ namespace jw::io
 
             struct
             {
-                chrono::tsc_count x0_min { 0 };
-                chrono::tsc_count x0_max { 0 };
-                chrono::tsc_count y0_min { 0 };
-                chrono::tsc_count y0_max { 0 };
-                chrono::tsc_count x1_min { 0 };
-                chrono::tsc_count x1_max { 0 };
-                chrono::tsc_count y1_min { 0 };
-                chrono::tsc_count y1_max { 0 };
+                chrono::tsc_count x0_min { std::numeric_limits<chrono::tsc_count>::max() };
+                chrono::tsc_count x0_max { std::numeric_limits<chrono::tsc_count>::max() };
+                chrono::tsc_count y0_min { std::numeric_limits<chrono::tsc_count>::max() };
+                chrono::tsc_count y0_max { std::numeric_limits<chrono::tsc_count>::max() };
+                chrono::tsc_count x1_min { std::numeric_limits<chrono::tsc_count>::max() };
+                chrono::tsc_count x1_max { std::numeric_limits<chrono::tsc_count>::max() };
+                chrono::tsc_count y1_min { std::numeric_limits<chrono::tsc_count>::max() };
+                chrono::tsc_count y1_max { std::numeric_limits<chrono::tsc_count>::max() };
             } calibration;
 
             struct
@@ -59,22 +60,37 @@ namespace jw::io
         struct value_t
         {
             T x0, y0, x1, y1;
+            constexpr value_t(T vx0, T vy0, T vx1, T vy1) noexcept : x0(vx0), y0(vy0), x1(vx1), y1(vy1) { }
+            constexpr value_t(T v) noexcept : value_t(v, v, v, v) { }
+            constexpr value_t() noexcept = default;
         };
 
         using raw_t = value_t<chrono::tsc_count>;
         using normalized_t = value_t<float>;
+
+        struct button_t
+        {
+            bool a0, b0, a1, b1;
+        };
 
         gameport(config c) : cfg(c), port(c.port) { }    // TODO
         ~gameport() { }
 
         auto get_raw()
         {
-            return raw_t { };   // TODO
+            poll();
+            return last;
         }
 
         auto get()
         {
+            poll();
             return normalized_t { };     // TODO
+        }
+
+        auto buttons()
+        {
+            return button_t { };    // TODO
         }
 
     private:
@@ -91,15 +107,42 @@ namespace jw::io
         };
         static_assert(sizeof(raw_gameport) == 1);
 
-        config cfg;
+        const config cfg;
         io_port<raw_gameport> port;
+        raw_t last;
+        value_t<bool> timing { false };
+        chrono::tsc_count timing_start;
 
-        thread::task<void()> poll_task { [] 
+        void poll()
+        {
+            if (not timing.x0 and not timing.y0 and not timing.x1 and not timing.y1)
+            {
+                port.write({ });
+                timing_start = chrono::rdtsc();
+            }
+            do
+            {
+                auto p = port.read();
+                auto now = chrono::rdtsc();
+                if (timing.x0 and not p.x0) { timing.x0 = false; if (now <= cfg.calibration.x0_max) last.x0 = now - timing_start; }
+                if (timing.y0 and not p.y0) { timing.y0 = false; if (now <= cfg.calibration.y0_max) last.y0 = now - timing_start; }
+                if (timing.x1 and not p.x1) { timing.x1 = false; if (now <= cfg.calibration.x1_max) last.x1 = now - timing_start; }
+                if (timing.y1 and not p.y1) { timing.y1 = false; if (now <= cfg.calibration.y1_max) last.y1 = now - timing_start; }
+            } while (cfg.strategy == poll_strategy::busy_loop and (timing.x0 or timing.y0 or timing.x1 or timing.y1));
+        }
+
+        thread::task<void()> poll_task { [this]
         {
             while (true)
             {
-                // TODO
+                poll();
+                thread::yield();
             }
-        } };
+        }, 1_KB };
+
+        dpmi::irq_handler poll_irq { [this]
+        {
+            poll();
+        }, dpmi::always_call };
     };
 }
