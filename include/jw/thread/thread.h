e4f2dce23d6332621364b14def178cf54126279a
J.W. Jagersma <jwjagersma@gmail.com>
2018-07-26 01:01:43 +0200

    remove forward declaration of jw::chrono from thread.h, as these are now already declared in jwdpmi_config.h

--- include/jw/thread/thread.h
+++ include/jw/thread/thread.h
@@ -1,4 +1,5 @@
 /* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
+/* Copyright (C) 2018 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2017 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2016 J.W. Jagersma, see COPYING.txt for details */
 
@@ -10,11 +11,6 @@
 
 namespace jw
 {
-    namespace chrono
-    {
-        struct pit;
-    }
-
     namespace thread
     {
         // Thrown when task->abort() is called.
