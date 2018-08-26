843af433bea2b25395f27433efc6bc35e4ac0342
J.W. Jagersma <jwjagersma@gmail.com>
2018-08-26 15:06:30 +0200

    matrix_range now supports creating sub-ranges from temporaries (eg. matrix.range(...).range(...)) and inverted ranges with negative dimensions.

--- include/jw/matrix.h
+++ include/jw/matrix.h
@@ -81,7 +81,7 @@ namespace jw
         mutable vector2i p;
     };
 
-    template<typename M>
+    template<typename R, typename T, unsigned L>
     struct matrix_range
     {
         using iterator = matrix_iterator<matrix_range, matrix_iterator_direction::right>;
@@ -93,8 +93,11 @@ namespace jw
         using const_vertical_iterator = matrix_iterator<const matrix_range, matrix_iterator_direction::down>;
         using const_reverse_vertical_iterator = matrix_iterator<const matrix_range, matrix_iterator_direction::up>;
 
-        constexpr matrix_range(M* matrix, const vector2i& position, const vector2i& dimensions) noexcept 
-            : m(matrix), pos(position), dim(dimensions) { }
+        template<typename, typename, unsigned> friend struct matrix_range;
+
+        template<typename R2>
+        constexpr matrix_range(R2&& range, const vector2i& position, const vector2i& dimensions) noexcept 
+            : r(std::forward<R2>(range)), pos(position), dim(dimensions) { }
 
         constexpr matrix_range() noexcept = delete;
         constexpr matrix_range(const matrix_range&) noexcept = default;
@@ -102,98 +105,131 @@ namespace jw
         constexpr matrix_range& operator=(const matrix_range&) noexcept = delete;
         constexpr matrix_range& operator=(matrix_range&&) noexcept = default;
 
-        constexpr auto range(vector2i position, vector2i dimensions) const noexcept
-        {
-            position = pos + vector2i::max_abs(position, vector2i { 0,0 });
-            dimensions = vector2i::min(std::abs(dimensions), size() - position).copysign(dimensions);
-            return matrix_range { m, position, dimensions };
-        }
+        constexpr auto range(vector2i position, vector2i dimensions) const noexcept { return make_range(*this, position, dimensions); }
+        constexpr auto range(vector2i position, vector2i dimensions) noexcept { return make_range(*this, position, dimensions); }
 
         constexpr auto range_abs(const vector2i& topleft, const vector2i& bottomright) const noexcept { return range(topleft, bottomright - topleft); }
+        constexpr auto range_abs(const vector2i& topleft, const vector2i& bottomright) noexcept { return range(topleft, bottomright - topleft); }
 
-        // no bounds checking
-        constexpr auto& operator()(vector2i p) noexcept { return get(p, m->data()); }
-        constexpr const auto& operator()(vector2i p) const noexcept { return get(p, m->data()); }
+        constexpr const auto& operator()(vector2i p) const noexcept { return get_wrap(p); }
+        constexpr auto& operator()(vector2i p) noexcept { return remove_const(get_wrap(p)); }
         constexpr const auto& operator()(std::ptrdiff_t x, std::ptrdiff_t y) const noexcept { return (*this)({ x, y }); }
         constexpr auto& operator()(std::ptrdiff_t x, std::ptrdiff_t y) noexcept { return (*this)({ x, y }); }
 
-        // bounds checking with automatic wrap-around
-        constexpr const auto& at(vector2i p) const noexcept { return get_wrap(p, m->data()); }
-        constexpr auto& at(vector2i p) noexcept { return get_wrap(p, m->data()); }
+        constexpr const auto& at(vector2i p) const noexcept { return get_wrap(p); }
+        constexpr auto& at(vector2i p) noexcept { return remove_const(get_wrap(p)); }
         constexpr const auto& at(std::ptrdiff_t x, std::ptrdiff_t y) const noexcept { return at({ x, y }); }
         constexpr auto& at(std::ptrdiff_t x, std::ptrdiff_t y) noexcept { return at({ x, y }); }
 
-        friend constexpr bool operator==(const matrix_range& lhs, const matrix_range& rhs) noexcept { return lhs.m.data() == rhs.m.data() && lhs.pos == rhs.pos && lhs.dim == rhs.dim; }
-        friend constexpr bool operator!=(const matrix_range& lhs, const matrix_range& rhs) noexcept { return !(lhs == rhs); }
+        constexpr const auto& nowrap(vector2i p) const noexcept { return get(p, r); }
+        constexpr auto& nowrap(vector2i p) noexcept { return remove_const(get(p)); }
+        constexpr const auto& nowrap(std::ptrdiff_t x, std::ptrdiff_t y) const noexcept { return nowrap({ x, y }); }
+        constexpr auto& nowrap(std::ptrdiff_t x, std::ptrdiff_t y) noexcept { return nowrap({ x, y }); }
+
+        template<typename R2, unsigned L2> friend constexpr bool operator==(const matrix_range& lhs, const matrix_range<R2, T, L2>& rhs) noexcept
+        {
+            return lhs.matrix().data() == rhs.matrix().data() and &lhs(0, 0) == &rhs(0, 0) and
+                &lhs(lhs.size() - vector2i { 1, 1 }) == &rhs(rhs.size() - vector2i { 1, 1 });
+        }
+        template<typename R2, unsigned L2> friend constexpr bool operator!=(const matrix_range& lhs, const matrix_range<R2, T, L2>& rhs) noexcept { return not (lhs == rhs); }
 
         constexpr matrix_range& fill(const auto& fill)
         {
-            for (auto y = 0; y < height(); ++y)
-                std::fill_n(&(*this)(0, y), width(), fill);
+            for (auto &&i : *this) i = fill;
             return *this;
         }
 
         constexpr matrix_range& assign(const auto& copy)
         {
-            for (auto y = 0; y < std::min(height(), copy.height()); ++y)
-                std::copy_n(&copy(0, y), std::min(width(), copy.width()), &(*this)(0, y));
+            auto size = vector2i::min(this->size(), copy.size());
+            for (auto y = 0; y < size[1]; ++y)
+                for (auto x = 0; x < size[0]; ++x)
+                    at(x, y) = copy.at(x, y);
             return *this;
         }
 
         constexpr auto begin() noexcept   { return iterator { *this, { 0, 0 } }; }
         constexpr auto vbegin() noexcept  { return vertical_iterator { *this, { 0, 0 } }; }
-        constexpr auto rbegin() noexcept  { return vertical_iterator { *this, dim - vector2i { 1, 1 } }; }
-        constexpr auto rvbegin() noexcept { return reverse_vertical_iterator { *this, dim - vector2i { 1, 1 } }; }
+        constexpr auto rbegin() noexcept  { return vertical_iterator { *this, size() - vector2i { 1, 1 } }; }
+        constexpr auto rvbegin() noexcept { return reverse_vertical_iterator { *this, size() - vector2i { 1, 1 } }; }
         constexpr auto cbegin() const noexcept   { return const_iterator { *this, { 0, 0 } }; }
         constexpr auto cvbegin() const noexcept  { return const_vertical_iterator { *this, { 0, 0 } }; }
-        constexpr auto crbegin() const noexcept  { return const_vertical_iterator { *this, dim - vector2i { 1, 1 } }; }
-        constexpr auto crvbegin() const noexcept { return const_reverse_vertical_iterator { *this, dim - vector2i { 1, 1 } }; }
+        constexpr auto crbegin() const noexcept  { return const_vertical_iterator { *this, size() - vector2i { 1, 1 } }; }
+        constexpr auto crvbegin() const noexcept { return const_reverse_vertical_iterator { *this, size() - vector2i { 1, 1 } }; }
         constexpr auto end() const noexcept { return invalid_matrix_iterator { }; }
 
-        constexpr const auto& position() const noexcept { return pos; }
-        constexpr auto size() const noexcept { return std::abs(dim); }
-        constexpr auto width() const noexcept { return size().x; }
-        constexpr auto height() const noexcept { return size().y; }
+        [[gnu::const]] constexpr const auto& position() const noexcept { return pos; }
+        [[gnu::const]] constexpr auto size() const noexcept { return std::abs(dim); }
+        [[gnu::const]] constexpr auto width() const noexcept { return size().x; }
+        [[gnu::const]] constexpr auto height() const noexcept { return size().y; }
 
-    protected:
-        constexpr auto& get_wrap(vector2i p, auto* ptr) const
+        template<unsigned level = L> constexpr auto& matrix() const noexcept
         {
-            p.v %= size().v;
-            if (p.x < 0) p.x = dim.x + p.x;
-            if (p.y < 0) p.y = dim.y + p.y;
-            return get(p, ptr);
+            if constexpr (level == 0) return r;
+            else return r.template matrix<level - 1>();
         }
 
-        constexpr auto& get(vector2i p, auto* ptr) const
+    protected:
+        template<typename Self>
+        constexpr auto make_range(Self&& self, vector2i position, vector2i dimensions) const noexcept
+        {
+            auto dim_sign = dim.sign();
+            dimensions[0] *= dim_sign[0]; dimensions[1] *= dim_sign[1];
+            return matrix_range<std::remove_reference_t<Self>, T, L + 1> { std::forward<Self>(self), position, dimensions };
+        }
+
+        constexpr T& remove_const(const T& t) const noexcept { return const_cast<T&>(t); }
+
+        constexpr const T& get_wrap(vector2i p) const
+        {
+            auto s = this->size();
+            p.v %= s.v;
+            p += s;
+            p.v %= s.v;
+            p.copysign(dim);
+            p += pos;
+            if constexpr (L > 0) return r.get_wrap(p);
+            else return r.base_get(p);
+        }
+
+        constexpr const T& get(vector2i p) const
         {
             p.copysign(dim);
             p += pos;
-            return *(ptr + p.x + m->size().x * p.y);
+            if constexpr (L > 0) return r.get(p);
+            else return r.base_get(p);
         }
 
-        M* m;
+        std::conditional_t<(L < 2), R&, R> r;
         vector2i pos, dim;
     };
 
     template<typename T>
-    struct matrix : public matrix_range<matrix<T>>
+    struct matrix : public matrix_range<matrix<T>, T, 0>
     {
-        constexpr matrix(vector2i size, T* data) : matrix_range<matrix<T>>(this, { 0, 0 }, size), p(data) { }
+        template<typename, typename, unsigned> friend struct matrix_range;
+
+        constexpr matrix(vector2i size, T* data) : matrix_range<matrix<T>, T, 0>(*this, { 0, 0 }, size), ptr(data) { }
         constexpr matrix(std::size_t w, std::size_t h, T* data) : matrix(vector2i { w, h }, data) { }
 
-        constexpr auto* data() noexcept { return p; }
+        constexpr auto* data() noexcept { return ptr; }
         constexpr const auto* data() const noexcept { return data(); }
         constexpr auto data_size() const noexcept { return this->width() * this->height(); }
 
     protected:
-        T* p;
+        constexpr const T& base_get(vector2i p) const
+        {
+            return *(ptr + p[0] + this->size()[0] * p[1]);
+        }
+
+        T* ptr;
     };
 
     template<typename T>
     struct matrix_container : public matrix<T>
     {
         matrix_container(vector2i size, std::experimental::pmr::memory_resource* memres = std::experimental::pmr::get_default_resource()) 
-            : matrix<T>(size, nullptr), data(size.x * size.y, std::experimental::pmr::polymorphic_allocator<T> { memres }) { this->p = data.data(); }
+            : matrix<T>(size, nullptr), data(size.x * size.y, std::experimental::pmr::polymorphic_allocator<T> { memres }) { this->ptr = data.data(); }
         matrix_container(std::size_t w, std::size_t h, std::experimental::pmr::memory_resource* memres = std::experimental::pmr::get_default_resource())
             : matrix_container(vector2i { w,h }, memres) { }
 
