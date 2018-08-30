92d181493c8829e20616e79c2f6e170ce9c85c1e
J.W. Jagersma <jwjagersma@gmail.com>
2018-08-30 19:23:27 +0200

    pixel: some refactoring to reduce code duplication.

--- include/jw/video/pixel.h
+++ include/jw/video/pixel.h
@@ -63,14 +63,10 @@ namespace jw
             using T = float;
             T b, g, r, a;
 
-            bgra_ffff() noexcept = default;
-            constexpr bgra_ffff(T cr, T cg, T cb, T ca) noexcept : b(cb), g(cg), r(cr), a(ca) { }
-
             static constexpr T rx = 1.0f;
             static constexpr T gx = 1.0f;
             static constexpr T bx = 1.0f;
             static constexpr T ax = 1.0f;
-            static constexpr bool has_alpha = true;
         };
 
         struct alignas(4) bgra_8888
@@ -78,14 +74,10 @@ namespace jw
             using T = std::uint8_t;
             T b, g, r, a;
 
-            bgra_8888() noexcept = default;
-            constexpr bgra_8888(T cr, T cg, T cb, T ca) noexcept : b(cb), g(cg), r(cr), a(ca) { }
-
             static constexpr T rx = 255;
             static constexpr T gx = 255;
             static constexpr T bx = 255;
             static constexpr T ax = 255;
-            static constexpr bool has_alpha = true;
         };
 
         struct [[gnu::packed]] bgra_8880
@@ -93,14 +85,10 @@ namespace jw
             using T = std::uint8_t;
             T b, g, r, a;
 
-            bgra_8880() noexcept = default;
-            constexpr bgra_8880(T cr, T cg, T cb, T) noexcept : b(cb), g(cg), r(cr), a(0) { }
-
             static constexpr T rx = 255;
             static constexpr T gx = 255;
             static constexpr T bx = 255;
             static constexpr T ax = 0;
-            static constexpr bool has_alpha = false;
         };
 
         struct [[gnu::packed]] bgr_8880
@@ -108,14 +96,10 @@ namespace jw
             using T = std::uint8_t;
             T b, g, r;
 
-            bgr_8880() noexcept = default;
-            constexpr bgr_8880(T cr, T cg, T cb, T) noexcept : b(cb), g(cg), r(cr) { }
-
             static constexpr T rx = 255;
             static constexpr T gx = 255;
             static constexpr T bx = 255;
             static constexpr T ax = 0;
-            static constexpr bool has_alpha = false;
         };
 
         struct [[gnu::packed]] bgra_6668
@@ -126,14 +110,10 @@ namespace jw
             T r : 6, : 2;
             T a : 8;
 
-            bgra_6668() noexcept = default;
-            constexpr bgra_6668(T cr, T cg, T cb, T ca) noexcept : b(cb), g(cg), r(cr), a(ca) { }
-
             static constexpr T rx = 63;
             static constexpr T gx = 63;
             static constexpr T bx = 63;
             static constexpr T ax = 255;
-            static constexpr bool has_alpha = true;
         };
 
         struct alignas(2) [[gnu::packed]] bgr_5650
@@ -143,14 +123,10 @@ namespace jw
             T g : 6;
             T r : 5;
 
-            bgr_5650() noexcept = default;
-            constexpr bgr_5650(T cr, T cg, T cb, T) noexcept : b(cb), g(cg), r(cr) { }
-
             static constexpr T rx = 31;
             static constexpr T gx = 63;
             static constexpr T bx = 31;
             static constexpr T ax = 0;
-            static constexpr bool has_alpha = false;
         };
 
         struct alignas(2) [[gnu::packed]] bgra_5551
@@ -161,14 +137,10 @@ namespace jw
             T r : 5;
             T a : 1;
 
-            bgra_5551() noexcept = default;
-            constexpr bgra_5551(T cr, T cg, T cb, T ca) noexcept : b(cb), g(cg), r(cr), a(ca) { }
-
             static constexpr T rx = 31;
             static constexpr T gx = 31;
             static constexpr T bx = 31;
             static constexpr T ax = 1;
-            static constexpr bool has_alpha = true;
         };
 
         struct alignas(2) [[gnu::packed]] bgra_5550
@@ -177,16 +149,12 @@ namespace jw
             T b : 5;
             T g : 5;
             T r : 5;
-            T a : 1;
-
-            bgra_5550() noexcept = default;
-            constexpr bgra_5550(T cr, T cg, T cb, T) noexcept : b(cb), g(cg), r(cr), a(0) { }
+            T : 1;
 
             static constexpr T rx = 31;
             static constexpr T gx = 31;
             static constexpr T bx = 31;
             static constexpr T ax = 0;
-            static constexpr bool has_alpha = false;
         };
 
         struct px { };
@@ -209,8 +177,15 @@ namespace jw
             };
 
             constexpr pixel() noexcept = default;
-            constexpr pixel(T cr, T cg, T cb, T ca) noexcept : P(cr, cg, cb, ca) { }
-            constexpr pixel(T cr, T cg, T cb) noexcept : P(cr, cg, cb, P::ax) { }
+            template<typename U = P, std::enable_if_t<pixel<U>::has_alpha(), bool> = { }>
+            constexpr pixel(T cr, T cg, T cb, T ca) noexcept : P { cb, cg, cr, ca } { }
+            template<typename U = P, std::enable_if_t<pixel<U>::has_alpha(), bool> = { } >
+            constexpr pixel(T cr, T cg, T cb) noexcept : P { cb, cg, cr, P::ax } { }
+            template<typename U = P, std::enable_if_t<not pixel<U>::has_alpha(), bool> = { } >
+            constexpr pixel(T cr, T cg, T cb, T) noexcept : P { cb, cg, cr } { }
+            template<typename U = P, std::enable_if_t<not pixel<U>::has_alpha(), bool> = { } >
+            constexpr pixel(T cr, T cg, T cb) noexcept : P { cb, cg, cr } { }
+
             constexpr pixel(const pixel& p) noexcept = default;
             constexpr pixel(pixel&& p) noexcept = default;
 
@@ -220,37 +195,35 @@ namespace jw
             constexpr pixel& operator=(const pixel& other) noexcept { return blend(other); }
             constexpr pixel& operator=(pixel&& o) noexcept = default;
 
-            template<typename V>
             constexpr pixel& assign_round(const auto& v) noexcept
             { 
-                if constexpr (std::is_integral<typename V::T>::value)
+                if constexpr (std::is_integral_v<typename P::T>)
                 {
                     this->b = jw::round(v.b);
                     this->g = jw::round(v.g);
                     this->r = jw::round(v.r);
-                    if constexpr (V::has_alpha) this->a = jw::round(v.a);
+                    if constexpr (has_alpha()) this->a = jw::round(v.a);
                     return *this;
                 }
                 this->b = v.b;
                 this->g = v.g;
                 this->r = v.r;
-                if constexpr (V::has_alpha) this->a = v.a;
+                if constexpr (has_alpha()) this->a = v.a;
                 return *this;
             }
 
-            template <typename U = P>
-            static constexpr pixel<U> m64(auto value) noexcept
+            static constexpr pixel m64(auto value) noexcept
             {
                 auto v = reinterpret_cast<V<8, byte>>(_mm_packs_pu16(value, _mm_setzero_si64()));
-                if constexpr (byte_aligned<U>())
+                if constexpr (byte_aligned())
                 {
-                    pixel<U> result { std::move(reinterpret_cast<pixel<U>&&>(v)) };
+                    pixel result { std::move(reinterpret_cast<pixel&&>(v)) };
                     _mm_empty();
                     return result;
                 }
                 else
                 {
-                    pixel<U> result { v[2], v[1], v[0], v[3] };
+                    pixel result { v[2], v[1], v[0], v[3] };
                     _mm_empty();
                     return result;
                 }
@@ -264,11 +237,10 @@ namespace jw
                 else return ret(_mm_setr_pi8(this->b, this->g, this->r, 0, 0, 0, 0, 0));
             }
 
-            template <typename U = P>
-            static constexpr pixel<U> m128(__m128 value) noexcept
+            static constexpr pixel m128(__m128 value) noexcept
             {
-                if constexpr (std::is_floating_point_v<typename U::T>) return pixel<U> { std::move(reinterpret_cast<pixel<U>&&>(value)) };
-                else return m64<U>(_mm_cvtps_pi16(value));
+                if constexpr (std::is_floating_point_v<typename P::T>) return *reinterpret_cast<pixel*>(value);
+                else return m64(_mm_cvtps_pi16(value));
             }
 
             constexpr __m128 m128() const noexcept
@@ -277,44 +249,43 @@ namespace jw
                 else return _mm_cvtpu16_ps(m64());
             }
 
-            template<typename U>
             static constexpr auto vector_shift(std::uint16_t noalpha = 255) noexcept
             {
-                return reinterpret_cast<__m64>((V<4, std::uint16_t> { U::bx, U::gx, U::rx, static_cast<std::uint16_t>(U::has_alpha ? U::ax : noalpha) } + 1) / 8);
+                return reinterpret_cast<__m64>((V<4, std::uint16_t> { P::bx, P::gx, P::rx, static_cast<std::uint16_t>(has_alpha() ? P::ax : noalpha) } + 1) / 8);
             }
 
-            template<typename U>
             static constexpr auto vector_max(float noalpha = 1) noexcept
             {
-                return reinterpret_cast<__m128>(V<4, float> { U::bx, U::gx, U::rx, U::has_alpha ? U::ax : noalpha });
+                return reinterpret_cast<__m128>(V<4, float> { P::bx, P::gx, P::rx, has_alpha() ? P::ax : noalpha });
             }
 
-            template<typename U = P>
             static constexpr bool byte_aligned()
             {
-                return std::is_same_v<U, bgra_8888> or std::is_same_v<U, bgra_8880> or std::is_same_v<U, bgra_6668> or std::is_same_v<U, bgr_8880>;
+                return std::is_same_v<P, bgra_8888> or std::is_same_v<P, bgra_8880> or std::is_same_v<P, bgra_6668> or std::is_same_v<P, bgr_8880>;
             }
+
+            static constexpr bool has_alpha() { return P::ax > 0; }
 
             template <typename U>
             constexpr pixel<U> cast_to() const noexcept
             {
                 if constexpr (std::is_floating_point_v<typename P::T> or std::is_floating_point_v<typename U::T>)
                 {
-                    constexpr __m128 maxp = vector_max<P>(1);
-                    constexpr __m128 maxu = vector_max<U>(0);
+                    constexpr __m128 maxp = vector_max(1);
+                    constexpr __m128 maxu = pixel<U>::vector_max(0);
 
                     __m128 src = m128();
                     src = _mm_div_ps(_mm_mul_ps(src, maxu), maxp);
-                    return m128<U>(src);
+                    return pixel<U>::m128(src);
                 }
                 else
                 {
-                    constexpr __m64 maxp = vector_shift<P>(255);
-                    constexpr __m64 maxu = vector_shift<U>(255);
+                    constexpr __m64 maxp = vector_shift(255);
+                    constexpr __m64 maxu = pixel<U>::vector_shift(255);
 
                     __m64 src = m64();
                     src = _mm_srl_pi16(_mm_sll_pi16(src, maxu), maxp);
-                    return m64<U>(src);
+                    return pixel<U>::m64(src);
                 }
             }
 
@@ -323,14 +294,14 @@ namespace jw
             template <typename U, std::enable_if_t<(std::is_floating_point<typename U::T>::value || std::is_floating_point<typename P::T>::value), bool> = { }> 
             static constexpr float max(auto max) noexcept { return max; }
 
-            template<typename V, typename U, std::enable_if_t<!U::has_alpha, bool> = { }>
+            template<typename V, typename U, std::enable_if_t<not pixel<U>::has_alpha(), bool> = { }>
             constexpr pixel& blend(const pixel<U>& src)
             {
                 auto copy = src.template cast_to<V>();
                 return assign_round<V>(copy);
             }
 
-            template<typename V, typename U, std::enable_if_t<!V::has_alpha && U::has_alpha, bool> = { }>
+            template<typename V, typename U, std::enable_if_t<not pixel<V>::has_alpha() and pixel<U>::has_alpha(), bool> = { }>
             constexpr pixel& blend(const pixel<U>& other)
             {
                 using max_T = decltype(max<U>(U::ax));
@@ -357,7 +328,7 @@ namespace jw
                         dest.v *= ax.v;
                         dest.v += src.v;
                         dest.v /= maxa.v;
-                        return assign_round<V>(dest);
+                        return assign_round(dest);
                     }
                     else
                     {
@@ -370,7 +341,7 @@ namespace jw
                         dest.v *= ax.v;
                         dest.v += src.v;
                         dest.v /= maxa.v;
-                        return assign_round<V>(dest);
+                        return assign_round(dest);
                     }
                 }
                 else
@@ -385,18 +356,18 @@ namespace jw
                     vec dest { this->r, this->g, this->b, 0 };
                     vec srca { src.a, src.a, src.a, src.a };
 
-                    if constexpr (not std::is_same<V, U>::value) src.v *= maxp.v;
+                    if constexpr (not std::is_same_v<V, U>) src.v *= maxp.v;
                     src.v *= srca.v;
-                    if constexpr (not std::is_same<V, U>::value) src.v /= maxu.v;
+                    if constexpr (not std::is_same_v<V, U>) src.v /= maxu.v;
                     ax.v -= srca.v;
                     dest.v *= ax.v;
                     dest.v += src.v;
                     dest.v /= maxa.v;
-                    return assign_round<V>(dest);
+                    return assign_round(dest);
                 }
             }
 
-            template<typename V, typename U, std::enable_if_t<V::has_alpha && U::has_alpha, bool> = { }>
+            template<typename V, typename U, std::enable_if_t<pixel<V>::has_alpha() and pixel<U>::has_alpha(), bool> = { }>
             constexpr pixel& blend(const pixel<U>& other)
             {
                 using max_T = decltype(max<U>(U::ax));
@@ -423,7 +394,7 @@ namespace jw
                         dest.v *= ax.v;
                         dest.v += src.v;
                         dest.v /= maxa.v;
-                        return assign_round<V>(dest);
+                        return assign_round(dest);
                     }
                     else
                     {
@@ -436,7 +407,7 @@ namespace jw
                         dest.v *= ax.v;
                         dest.v += src.v;
                         dest.v /= maxa.v;
-                        return assign_round<V>(dest);
+                        return assign_round(dest);
                     }
                 }
                 else
@@ -451,14 +422,14 @@ namespace jw
                     vec dest { this->r, this->g, this->b, this->a };
                     vec srca { src.a, src.a, src.a, src.a };
 
-                    if (!std::is_same<V, U>::value) src.v *= maxp.v;
+                    if constexpr (not std::is_same_v<V, U>) src.v *= maxp.v;
                     src.v *= srca.v;
-                    if (!std::is_same<V, U>::value) src.v /= maxu.v;
+                    if constexpr (not std::is_same_v<V, U>) src.v /= maxu.v;
                     ax.v -= srca.v;
                     dest.v *= ax.v;
                     dest.v += src.v;
                     dest.v /= maxa.v;
-                    return assign_round<V>(dest);
+                    return assign_round(dest);
                 }
             }
 
