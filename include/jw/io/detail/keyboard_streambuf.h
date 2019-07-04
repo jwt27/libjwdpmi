a8f02d3e0bd668cd74791f0f1efb912f1cd62cb8
J.W. Jagersma <jwjagersma@gmail.com>
2019-07-05 01:14:15 +0200

    moved keyboard_streambuf::event_handler to cpp.

--- include/jw/io/detail/keyboard_streambuf.h
+++ include/jw/io/detail/keyboard_streambuf.h
@@ -4,6 +4,8 @@
 /* Copyright (C) 2017 J.W. Jagersma, see COPYING.txt for details */
 
 #pragma once
+#include <functional>
+#include <array>
 #include <jw/io/keyboard.h>
 
 namespace jw
@@ -34,65 +36,7 @@ namespace jw
 
             private:
                 callback<bool(key, key_state)> event_callback { std::bind_front(&keyboard_streambuf::event_handler, this) };
-                bool event_handler(key k, key_state s)
-                {
-                    auto insert = [this](char c)
-                    {
-                        *(ptr++) = c;
-                        if (echo)
-                        {
-                            *echo_stream << c;
-                            if (c == '\b')* echo_stream << " \b";
-                            *echo_stream << std::flush;
-                        }
-                        setg(buffer.begin(), gptr(), ptr);
-                    };
-
-                    if (egptr() >= buffer.end()) sync();
-
-                    if (k == key::any_alt and s == key_state::up)
-                    {
-                        if (alt_sequence <= 0xff) insert(alt_sequence);
-                        alt_sequence = 0x80000000;
-                        return true;
-                    }
-
-                    if (s.is_up()) return false;
-
-                    if (keyb[key::any_alt])
-                    {
-                        if (not (keyb[key::any_shift] xor keyb[key::num_lock_state])) return false;
-                        if (keyb[key::any_ctrl]) return false;
-                        auto n = [k]
-                        {
-                            switch (k)
-                            {
-                            case key::num_0: return 0;
-                            case key::num_1: return 1;
-                            case key::num_2: return 2;
-                            case key::num_3: return 3;
-                            case key::num_4: return 4;
-                            case key::num_5: return 5;
-                            case key::num_6: return 6;
-                            case key::num_7: return 7;
-                            case key::num_8: return 8;
-                            case key::num_9: return 9;
-                            default: return -1;
-                            }
-                        }();
-                        if (n == -1) return false;
-                        alt_sequence *= 10;
-                        alt_sequence += n;
-                        return true;
-                    }
-
-                    if (auto c = k.to_ascii(keyb))
-                    {
-                        insert(c);
-                        return true;
-                    }
-                    return false;
-                }
+                bool event_handler(key k, key_state s);
 
                 std::array<char_type, 1_KB> buffer;
                 std::uint32_t alt_sequence { 0x80000000 };
