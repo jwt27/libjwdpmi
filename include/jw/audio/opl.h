2166d4a92d555cba4acf4de03e9a26bbe1d66bd7
J.W. Jagersma <jwjagersma@gmail.com>
2020-11-11 01:54:56 +0100

    add OPL interface

---
+++ include/jw/audio/opl.h
@@ -0,0 +1,202 @@
+/* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
+/* Copyright (C) 2020 J.W. Jagersma, see COPYING.txt for details */
+
+#pragma once
+#include <bitset>
+#include <array>
+#include <cstring>
+#include <jw/io/ioport.h>
+#include <jw/chrono/chrono.h>
+#include <jw/thread/thread.h>
+#include <../jwdpmi_config.h>
+
+namespace jw::audio
+{
+    enum class opl_type
+    {
+        unknown, opl2, opl3, opl3_l
+    };
+
+    struct basic_opl final
+    {
+        using clock = config::thread_clock;
+
+        struct [[gnu::packed]] common_registers
+        {
+            unsigned test : 8;
+            unsigned timer0 : 8;
+            unsigned timer1 : 8;
+            bool start_timer0 : 1;
+            bool start_timer1 : 1;
+            unsigned : 3;
+            bool mask_timer1 : 1;
+            bool mask_timer0 : 1;
+            bool reset_irq : 1;
+            unsigned : 6;
+            bool note_sel : 1;
+            bool enable_composite_sine_mode : 1;
+            bool hihat : 1;
+            bool top_cymbal : 1;
+            bool tomtom : 1;
+            bool snare_drum : 1;
+            bool bass_drum : 1;
+            bool enable_percussion : 1;
+            unsigned vibrato_depth : 1;
+            unsigned tremolo_depth : 1;
+            unsigned test_opl3 : 6;
+            unsigned : 2;
+            struct [[gnu::packed]]
+            {
+                bool ch0 : 1;
+                bool ch1 : 1;
+                bool ch2 : 1;
+                bool ch9 : 1;
+                bool chA : 1;
+                bool chB : 1;
+                unsigned : 2;
+
+                void bitset(std::bitset<6> value) noexcept { *reinterpret_cast<std::uint8_t*>(this) = value.to_ulong(); }
+                constexpr std::bitset<6> bitset() const noexcept { return { *reinterpret_cast<const std::uint8_t*>(this) }; }
+            } enable_4op;
+            bool enable_opl3 : 1;
+            unsigned : 1;
+            bool enable_opl3_l : 1;
+            unsigned : 5;
+        };
+        static_assert(sizeof(common_registers) == 9);
+
+        struct [[gnu::packed]] oscillator
+        {
+            unsigned multiplier : 4;
+            bool key_scale_rate : 1;
+            bool enable_sustain : 1;
+            bool vibrato : 1;
+            bool tremolo : 1;
+            unsigned attenuation : 6;
+            unsigned key_scale_level : 2;
+            unsigned decay : 4;
+            unsigned attack : 4;
+            unsigned release : 4;
+            unsigned sustain : 4;
+            unsigned waveform : 3;
+            unsigned : 5;
+        };
+        static_assert(sizeof(oscillator) == 5);
+
+        struct [[gnu::packed]] channel
+        {
+            unsigned connection : 1;
+            unsigned feedback : 3;
+            bool output_ch0 : 1;    // left
+            bool output_ch1 : 1;    // right
+            bool output_ch2 : 1;
+            bool output_ch3 : 1;
+            unsigned freq_num : 10;
+            unsigned freq_block : 3;
+            bool key_on : 1;
+            unsigned : 2;
+
+            template<unsigned sample_rate = 49716>
+            void set_freq(float f)
+            {
+                constexpr auto calc_blocks = []
+                {
+                    std::array<unsigned, 8> result;
+                    for (int i = 0; i < 8; i++) result[i] = 1023 * sample_rate / (1 << (20 - i));
+                    return result;
+                };
+                constexpr std::array<unsigned, 8> block_maxfreq { calc_blocks() };
+
+                unsigned i;
+                for (i = 0; i < 8; ++i)
+                    if (f <= block_maxfreq[i]) break;
+
+                freq_block = i;
+                freq_num = static_cast<unsigned>(f * (1 << (20 - i))) / sample_rate;
+            }
+
+            void output(std::bitset<4> value) noexcept
+            {
+                auto* const p = reinterpret_cast<std::uint8_t*>(this);
+                *p &= 0x0f;
+                *p |= value.to_ulong() << 4;
+            }
+            constexpr std::bitset<4> output() const noexcept
+            {
+                return { static_cast<unsigned>(*reinterpret_cast<const std::uint8_t*>(this) >> 4) };
+            }
+        };
+        static_assert(sizeof(channel) == 3);
+
+        struct [[gnu::packed]] status_t
+        {
+            bool busy : 1;
+            bool opl2 : 1;
+            bool busy2 : 1;
+            unsigned : 2;
+            bool timer1 : 1;
+            bool timer0 : 1;
+            bool irq : 1;
+        };
+        static_assert(sizeof(status_t) == 1);
+
+        basic_opl(io::port_num port);
+        ~basic_opl() { reset(); }
+
+        void write(const common_registers& value);
+        void write(const oscillator& value, std::uint8_t slot);
+        void write(const oscillator& value, std::uint8_t ch, std::uint8_t osc) { write(value, oscillator_slot(ch, osc)); }
+        void write(const channel& value, std::uint8_t ch);
+        common_registers read() const noexcept { return common.value; }
+        status_t status() const noexcept { return status_register.read(); }
+        void reset();
+
+        // Return absolute oscillator slot number for given operator in given channel.
+        static constexpr std::uint8_t oscillator_slot(std::uint8_t ch, std::uint8_t osc) noexcept
+        {
+            return ch + 3 * (ch / 3) + 3 * osc;
+        }
+
+    private:
+        basic_opl(const basic_opl&) = delete;
+        basic_opl(basic_opl&&) = delete;
+        basic_opl& operator=(const basic_opl&) = delete;
+        basic_opl& operator=(basic_opl&&) = delete;
+
+        template <typename T>
+        struct reg
+        {
+            union
+            {
+                T value;
+                std::array<std::byte, sizeof(T)> raw;
+            };
+
+            constexpr reg() noexcept = default;
+            constexpr reg(const T& v) noexcept : value { v } { }
+        };
+
+        template <typename T>
+        struct cached_reg : reg<T>
+        {
+            std::bitset<sizeof(T)> written { };
+        };
+
+        template<unsigned... R, typename T> void write(const T& v, cached_reg<T>& cache, unsigned offset);
+        template<opl_type t> void do_write(std::uint16_t reg, std::byte value);
+        void write(std::uint16_t reg, std::byte value);
+        opl_type detect();
+
+        cached_reg<common_registers> common { };
+        std::array<cached_reg<oscillator>, 36> oscillators { };
+        std::array<cached_reg<channel>, 18> channels { };
+        clock::time_point last_access { };
+        io::in_port<status_t> status_register;
+        std::array<io::out_port<std::uint8_t>, 2> index;
+        std::array<std::uint8_t, 2> current_index;
+        std::array<io::io_port<std::byte>, 2> data;
+
+    public:
+        const opl_type type;
+    };
+}
---
+++ src/opl.cpp
@@ -0,0 +1,138 @@
+/* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
+/* Copyright (C) 2020 J.W. Jagersma, see COPYING.txt for details */
+
+#include <jw/audio/opl.h>
+#include <jw/io/io_error.h>
+
+namespace jw::audio
+{
+    basic_opl::basic_opl(io::port_num port)
+        : status_register { port }
+        , index { port + 0, port + 2 }
+        , data { port + 1, port + 3 }
+        , type { detect() }
+    {
+        common_registers c { };
+        c.mask_timer0 = true;
+        c.mask_timer1 = true;
+        c.enable_opl3 = type != opl_type::opl2;
+        c.enable_opl3_l = type == opl_type::opl3_l;
+        write(c);
+    }
+
+    void basic_opl::reset()
+    {
+        for (unsigned i = 0; i < 18; ++i)
+        {
+            channel c { channels[i].value };
+            c.key_on = false;
+            c.freq_block = 0;
+            c.freq_num = 0;
+            write(c, i);
+        }
+        common_registers c { };
+        c.mask_timer0 = true;
+        c.mask_timer1 = true;
+        write(c);
+    }
+
+    opl_type basic_opl::detect()
+    {
+        using namespace std::chrono_literals;
+        auto w = [this] (unsigned r, unsigned v) { do_write<opl_type::opl2>(r, static_cast<std::byte>(v)); };
+        auto w_opl3 = [this] (unsigned r, unsigned v) { do_write<opl_type::opl3>(r, static_cast<std::byte>(v)); };
+
+        // from https://www.fit.vutbr.cz/~arnost/opl/opl3.html#appendixB
+        w(0x04, 0x60);          // mask both timers
+        w(0x04, 0x80);          // reset irq
+        if (status().timer0) throw io::device_not_found { "OPL not detected" };
+        w(0x02, 0xff);          // set timer 0 count 0xff
+        w(0x04, 0x21);          // start timer 0
+        thread::yield_for(80us);
+        auto s = status();
+        if (not s.timer0) throw io::device_not_found { "OPL not detected" };
+        w(0x04, 0x60);          // mask both timers
+        w(0x04, 0x80);          // reset irq
+        if (s.opl2) return opl_type::opl2;
+
+        w_opl3(0x02, 0xa5);     // write a distinctive value to timer 0
+        thread::yield_for(2235ns);
+        if (data[0].read() == std::byte { 0xa5 }) return opl_type::opl3_l;
+        else return opl_type::opl3;
+    }
+
+    void basic_opl::write(const common_registers& value)
+    {
+        write<0x01, 0x02, 0x03, 0x04, 0x08, 0xbd, 0x101, 0x104, 0x105>(value, common, 0);
+        common.value.reset_irq = false;
+        common.value.hihat = false;
+        common.value.top_cymbal = false;
+        common.value.tomtom = false;
+        common.value.snare_drum = false;
+        common.value.bass_drum = false;
+    }
+
+    void basic_opl::write(const oscillator& value, std::uint8_t slot)
+    {
+        if (slot >= 18) write<0x120, 0x140, 0x160, 0x180, 0x1e0>(value, oscillators[slot], slot - 18);
+        else write<0x20, 0x40, 0x60, 0x80, 0xe0>(value, oscillators[slot], slot);
+    }
+
+    void basic_opl::write(const channel& value, std::uint8_t ch)
+    {
+        if (ch >= 9) write<0x1c0, 0x1a0, 0x1b0>(value, channels[ch], ch - 9);
+        else write<0xc0, 0xa0, 0xb0>(value, channels[ch], ch);
+    }
+
+    template<unsigned... R, typename T>
+    void basic_opl::write(const T& v, cached_reg<T>& cache, unsigned offset)
+    {
+        static_assert(sizeof...(R) == sizeof(T));
+        static constexpr unsigned regnum[] { R... };
+        const reg<T> value { v };
+        for (unsigned i = 0; i < sizeof(T); ++i)
+        {
+            if (value.raw[i] == cache.raw[i] and cache.written[i]) continue;
+            write(regnum[i] + offset, value.raw[i]);
+            cache.raw[i] = value.raw[i];
+        }
+    }
+
+    void basic_opl::write(std::uint16_t reg, std::byte value)
+    {
+        switch (type)
+        {
+        case opl_type::unknown: return do_write<opl_type::unknown>(reg, value);
+        case opl_type::opl2: return do_write<opl_type::opl2>(reg, value);
+        case opl_type::opl3: return do_write<opl_type::opl3>(reg, value);
+        case opl_type::opl3_l: return do_write<opl_type::opl3_l>(reg, value);
+        }
+    }
+
+    template<opl_type t>
+    void basic_opl::do_write(std::uint16_t reg, std::byte value)
+    {
+        using namespace std::chrono_literals;
+        constexpr bool opl2 = t == opl_type::opl2;
+        constexpr bool opl3 = t == opl_type::opl3;
+        constexpr bool opl3_l = t == opl_type::opl3_l;
+
+        const bool hi = reg > 0xff;
+        reg &= 0xff;
+        if constexpr (opl2) if (hi) return;
+
+        if constexpr (opl3_l) thread::yield_while([this] { return status().busy; });
+        else thread::yield_while([this] { return clock::now() < last_access + (opl3 ? 2235ns : 23us); });
+
+        if (current_index[hi] != reg) [[likely]]
+        {
+            index[hi].write(reg);
+            if constexpr (opl2) last_access = clock::now();
+            current_index[hi] = reg;
+            if constexpr (opl2) thread::yield_while([this] { return clock::now() < last_access + 3300ns; });
+        }
+
+        data[hi].write(value);
+        if constexpr (opl2 or opl3) last_access = clock::now();
+    }
+}
