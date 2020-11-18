3078cac62f28c8a118c2ac84b3dc5171853d55cd
J.W. Jagersma <jwjagersma@gmail.com>
2020-11-18 12:51:08 +0100

    clean up thread.h

--- include/jw/thread/thread.h
+++ include/jw/thread/thread.h
@@ -1,4 +1,5 @@
 /* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
+/* Copyright (C) 2020 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2018 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2017 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2016 J.W. Jagersma, see COPYING.txt for details */
@@ -14,45 +15,34 @@ namespace jw
     namespace thread
     {
         // Thrown when task->abort() is called.
-        struct abort_thread : public std::exception
+        struct abort_thread
         {
-            virtual const char* what() const noexcept override { return "Task aborted."; }
+            virtual const char* what() const noexcept { return "Task aborted"; }
         };
 
         // Thrown when a task is still running, but no longer referenced anywhere.
         // By default, threads may not be orphaned. This behaviour can be changed with task->allow_orphan.
         struct orphaned_thread : public abort_thread
         {
-            virtual const char* what() const noexcept override { return "Task orphaned, aborting."; }
+            virtual const char* what() const noexcept override { return "Task orphaned, aborting"; }
         };
 
         // Thrown when task->await() is called while no result will ever be available.
         struct illegal_await : public std::exception
         {
-            virtual const char* what() const noexcept override
-            {
-                std::stringstream s;
-                s << "Illegal call to await().\n";
-                s << "Thread" << thread->name << " (id " << std::dec << thread->id() << ")";
-                return s.str().c_str();
-            }
+            virtual const char* what() const noexcept override { return "Illegal call to await()"; }
+            illegal_await(const detail::thread_ptr& t) noexcept : thread { t } { }
+
             const detail::thread_ptr thread;
-            illegal_await(const detail::thread_ptr& t) noexcept : thread(t) { }
         };
 
         // Thrown on parent thread in a nested_exception when an unhandled exception occurs on a child thread.
         struct thread_exception : public std::exception
         {
-            virtual const char* what() const noexcept override
-            {
-                std::stringstream s;
-                s << "Exception thrown from thread";
-                if (auto t = thread.lock()) s << ": " << t->name << " (id " << std::dec << t->id() << ")";
-                else s << '.';
-                return s.str().c_str();
-            }
+            virtual const char* what() const noexcept override { return "Exception thrown from thread"; }
+            thread_exception(const detail::thread_ptr& t) noexcept : thread { t } { }
+
             const std::weak_ptr<detail::thread> thread;
-            thread_exception(const detail::thread_ptr& t) noexcept : thread(t) { }
         };
 
         // Yields execution to the next thread in the queue.
@@ -64,7 +54,7 @@ namespace jw
         }
 
         // Yields execution while the given condition evaluates to true.
-        inline void yield_while(auto&& condition)
+        template<typename F> inline void yield_while(F&& condition)
         {
             while (condition()) yield();
         };
@@ -82,15 +72,15 @@ namespace jw
         };
 
         // Combination of yield_while() and yield_until(). Returns true on timeout.
-        template<typename P> inline bool yield_while_until(auto&& condition, const P& time_point)
+        template<typename F, typename P> inline bool yield_while_until(F&& condition, const P& time_point)
         {
             bool c;
-            yield_while([&] { return (c = condition()) && P::clock::now() < time_point; });
+            yield_while([&] { return (c = condition()) and P::clock::now() < time_point; });
             return c;
         };
 
         // Combination of yield_while() and yield_for(). Returns true on timeout.
-        template<typename C = config::thread_clock> inline bool yield_while_for(auto&& condition, const typename C::duration& duration)
+        template<typename C = config::thread_clock, typename F> inline bool yield_while_for(F&& condition, const typename C::duration& duration)
         {
             return yield_while_until(condition, C::now() + duration);
         };
