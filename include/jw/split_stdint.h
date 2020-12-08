80521f14d2d2dc29f9dccee640b534825ec6d890
J.W. Jagersma <jwjagersma@gmail.com>
2020-12-08 16:48:38 +0100

    remove unnecessary specialization for split_int

--- include/jw/split_stdint.h
+++ include/jw/split_stdint.h
@@ -1,4 +1,5 @@
 /* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
+/* Copyright (C) 2020 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2019 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2018 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2017 J.W. Jagersma, see COPYING.txt for details */
@@ -15,7 +16,7 @@ namespace jw
         union split_int;
 
         template<typename T, std::size_t size>
-        union [[gnu::packed]] split_int<T, size, std::enable_if_t<(size / 2) % 2 == 0, bool>>
+        union [[gnu::packed]] split_int<T, size, std::enable_if_t<(size > 16) and (size / 2) % 2 == 0, bool>>
         {
             struct [[gnu::packed]]
             {
@@ -24,13 +25,14 @@ namespace jw
             };
             std::conditional_t<std::is_signed_v<T>, std::int64_t, std::uint64_t> value : size;
             constexpr split_int() noexcept = default;
-            constexpr split_int(auto v) noexcept : value(v) { };
-            template<typename L, typename H> constexpr split_int(L&& l, H&& h) noexcept : lo(std::forward<L>(l)), hi(std::forward<H>(h)) { };
+            constexpr split_int(auto v) noexcept : value { v } { };
+            template<typename L, typename H>
+            constexpr split_int(L&& l, H&& h) noexcept : lo { std::forward<L>(l) }, hi { std::forward<H>(h) } { };
             constexpr operator auto() const noexcept { return value; }
         };
 
         template<typename T, std::size_t size>
-        union [[gnu::packed]] split_int<T, size, std::enable_if_t<(size / 2) % 2 != 0, bool>>
+        union [[gnu::packed]] split_int<T, size, std::enable_if_t<((size <= 16) or (size / 2) % 2 != 0) and (size % 2) == 0, bool>>
         {
             struct [[gnu::packed]]
             {
@@ -40,16 +42,7 @@ namespace jw
             std::conditional_t<std::is_signed_v<T>, std::int64_t, std::uint64_t> value : size;
             constexpr split_int() noexcept = default;
             constexpr split_int(auto v) noexcept : value(v) { };
-            template<typename L, typename H> constexpr split_int(L&& l, H&& h) noexcept : lo(std::forward<L>(l)), hi(std::forward<H>(h)) { };
-            constexpr operator auto() const noexcept { return value; }
-        };
-
-        template<typename T>
-        union [[gnu::packed]] split_int<T, 8, bool>
-        {
-            std::conditional_t<std::is_signed<T>::value, std::int8_t, std::uint8_t> value;
-            constexpr split_int() noexcept = default;
-            constexpr split_int(auto v) noexcept : value(v) { }
+            constexpr split_int(auto l, auto h) noexcept : lo { l }, hi { h } { };
             constexpr operator auto() const noexcept { return value; }
         };
     }
@@ -63,14 +56,12 @@ namespace jw
     using split_int32_t = split_int<signed, 32>;
     using split_int64_t = split_int<signed, 64>;
 
-#ifndef __INTELLISENSE__
     static_assert(sizeof(split_uint64_t) == 8);
     static_assert(sizeof(split_uint32_t) == 4);
     static_assert(sizeof(split_uint16_t) == 2);
     static_assert(alignof(split_uint64_t) == 4);
     static_assert(alignof(split_uint32_t) == 4);
     static_assert(alignof(split_uint16_t) == 2);
-#endif
 }
 
 #pragma GCC diagnostic pop
