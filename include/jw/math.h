86730d5a66f1705400af3c4d4724d84e3af76da9
J.W. Jagersma <jwjagersma@gmail.com>
2021-10-20 00:04:58 +0200

    constrain integer round() with std::integral

--- include/jw/math.h
+++ include/jw/math.h
@@ -17,13 +17,13 @@ namespace jw
     template<typename U> constexpr inline auto remainder(double a, U b) { return __builtin_remainder(a, b); }
     template<typename U> constexpr inline auto remainder(float a, U b) { return __builtin_remainderf(a, b); }
 
-    //template<typename T, typename U> constexpr auto copysign(T a, U b) { return std::signbit(a) != std::signbit(b)? -a : a; }
     template<typename T, typename U> constexpr auto copysign(T a, U b) { return (a > 0) != (b > 0) ? -a : a; }
     template<typename U> constexpr inline auto copysign(long double a, U b) { return __builtin_copysignl(a, b); }
     template<typename U> constexpr inline auto copysign(double a, U b) { return __builtin_copysign(a, b); }
     template<typename U> constexpr inline auto copysign(float a, U b) { return __builtin_copysignf(a, b); }
 
-    template<typename T> constexpr inline auto round(T a) { return a; }
+    template<typename T> constexpr inline auto round(T a);
+    template<std::integral T> constexpr inline auto round(T a) { return a; }
     template<> constexpr inline auto round(long double a) { return __builtin_roundl(a); }
     template<> constexpr inline auto round(double a) { return __builtin_round(a); }
     template<> constexpr inline auto round(float a) { return __builtin_roundf(a); }
