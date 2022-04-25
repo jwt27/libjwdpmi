361eb41a5c82691276f29181037005fe2c2e1a4a
J.W. Jagersma <jwjagersma@gmail.com>
2022-04-25 16:40:49 +0200

    make dma_buffer a template

--- include/jw/io/dma.h
+++ include/jw/io/dma.h
@@ -8,22 +8,25 @@ namespace jw::io
 {
     // Allocates a buffer in conventional memory that does not cross any 64K
     // alignment boundaries, making it suitable for ISA DMA transfers.
+    template<typename T>
     struct dma_buffer
     {
-        dma_buffer(std::size_t size)
-            : mem(size + 64_KB)
+        dma_buffer(std::size_t num_elements)
+            : mem(sizeof(T) * num_elements + 64_KB)
         {
-            if (size > 64_KB) throw std::length_error { "DMA buffer too large" };
+            const std::size_t n = sizeof(T) * num_elements;
+            if (n > 64_KB) throw std::length_error { "DMA buffer too large" };
             const std::uintptr_t address = mem.dos_pointer().segment << 4;
             const std::uintptr_t aligned = (address + 0xffff) & 0xffff0000;
             offset = aligned - address;
-            if (offset >= size) offset = 0;
-            if (offset < 64_KB) mem.resize(offset + size);
+            if (offset >= n) offset = 0;
+            if (offset < 64_KB) mem.resize(offset + n);
         }
 
-        std::byte* pointer() const noexcept { return mem.near_pointer() + offset; }
+        T* pointer() const noexcept { return reinterpret_cast<T*>(mem.near_pointer() + offset); }
         std::uintptr_t physical_address() const noexcept { return (mem.dos_pointer().segment << 4) + offset; }
-        std::size_t size() const noexcept { return mem.size() - offset; }
+        std::size_t size_bytes() const noexcept { return mem.size() - offset; }
+        std::size_t size() const noexcept { return size_bytes() / sizeof(T); }
 
     private:
         dpmi::dos_memory<std::byte> mem;
