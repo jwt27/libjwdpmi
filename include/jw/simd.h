999959d74791a2bacbd8e17a536c1a357fe795ef
J.W. Jagersma <jwjagersma@gmail.com>
2022-10-27 23:06:24 +0200

    new idea: SIMD pipelines

--- include/jw/simd.h
+++ include/jw/simd.h
@@ -45,7 +45,10 @@ namespace jw
     concept simd_format = any_of<T, format_nosimd, format_pi8, format_pi16, format_pi32, format_si64, format_ps, format_pf>;
 
     template<typename T, typename... U>
-    concept specific_simd_format = simd_format<T> and (simd_format<U> and ...) and any_of<T, U...>;
+    concept any_simd_format_of = simd_format<T> and (simd_format<U> and ...) and any_of<T, U...>;
+
+    template<typename T, typename... U>
+    concept any_simd_format_but = simd_format<T> and (simd_format<U> and ...) and not any_of<T, U...>;
 
     template<simd_format>
     struct simd_format_traits
@@ -301,49 +304,171 @@ namespace jw
     template<typename F, simd flags, typename... A>
     concept simd_invocable = requires(F&& f, A&&... args) { std::forward<F>(f).template operator()<flags>(std::forward<A>(args)...); };
 
-    template<typename F, simd flags, typename... A>
-    using simd_invoke_result = decltype(std::declval<F>().template operator()<flags>(std::declval<A>()...));
-
-    template<simd flags, typename F, typename... A> requires (simd_invocable<F, flags, A...>)
-    [[gnu::always_inline, gnu::flatten]] decltype(auto) simd_invoke(F&& func, A&&... args)
+    template<simd flags, typename F, typename... A>
+    [[gnu::flatten]] decltype(auto) simd_invoke(F&& func, A&&... args)
     {
         return (std::forward<F>(func).template operator()<flags>(std::forward<A>(args)...));
     }
 
-    // Apply given transform function to src and store result in dst.  Size
-    // must be divisible by 8, and pointers must be 16-byte aligned.
-    template<simd flags, typename To, typename From, typename F, typename... A>
-    [[gnu::flatten, gnu::hot]] To* simd_transform(To* dst, const From* src, std::size_t n, F func, A... args)
-    {
-        constexpr std::type_identity<From> id { };
-        std::size_t i = 0;
+    template<typename F, simd flags, typename... A>
+    using simd_invoke_result = decltype(simd_invoke<flags>(std::declval<F>(), std::declval<A>()...));
 
+#   pragma GCC diagnostic push
+#   pragma GCC diagnostic ignored "-Wunused-local-typedefs"
+
+    // Wrap data in a struct with the specified type info.
+    template <typename T, typename D>
+    auto simd_data(D&& data)
+    {
+        struct impl
+        {
+            using type = T;
+            using data_type = std::remove_cvref_t<D>;
+            const data_type data;
+
+            constexpr operator data_type() const noexcept { return data; }
+        };
+        return impl { std::forward<D>(data) };
+    }
+
+    // Read the type name stored by simd_data().
+    template<typename D> using simd_type = typename std::remove_reference_t<D>::type;
+
+    // Wrap simd_data in a struct with the specified format info.
+    template<simd_format F, typename... D>
+    auto simd_return(F, D&&... data)
+    {
+        struct impl
+        {
+            using format = F;
+            std::tuple<std::remove_cvref_t<D>...> data;
+        };
+        return impl { std::tuple<std::remove_cvref_t<D>...> { simd_data<simd_type<D>, typename simd_type_traits<simd_type<D>, F>::data_type>(std::forward<D>(data))... } };
+    }
+
+#   pragma GCC diagnostic pop
+
+    // Increment an iterator by the amount specified in simd_type_traits.
+    template<simd_format F, typename I>
+    inline void increment_simd_iterator(I* i) noexcept
+    {
+        constexpr auto delta = simd_type_traits<std::iter_value_t<I>, F>::delta;
+        if constexpr (delta == 1) ++*i;
+        else *i += delta;
+    }
+
+    // Load from one or more iterators, passed by pointer, and return
+    // simd_data.  The iterators are incremented by the amount specified in
+    // simd_type_traits.
+    struct simd_source
+    {
+        template<simd flags, simd_format F, typename... I> requires (can_load<I, F> and ...)
+        auto operator()(F, I*... iterators)
+        {
+            auto ret = simd_return(F { }, simd_data<std::iter_value_t<I>>(simd_load(F { }, *iterators))...);
+            (increment_simd_iterator<F>(iterators), ...);
+            return ret;
+        }
+    };
+
+    // Store the given simd_data in a contained set of iterators.
+    template<typename... I>
+    struct simd_sink
+    {
+        constexpr simd_sink(I... i) : iterators { i... } { }
+
+        template<simd flags, simd_format F, typename... D>
+        requires (sizeof...(D) == sizeof...(I) and
+                  (can_store<I, F> and ...) and
+                  (std::output_iterator<I, simd_type<D>> and ...))
+        void operator()(F, D&&... data)
+        {
+            store(F { }, std::index_sequence_for<D...> { }, std::forward_as_tuple(std::forward<D>(data)...));
+        }
+
+    private:
+        template<simd_format F, std::size_t... N>
+        void store(F, std::index_sequence<N...>, auto data)
+        {
+            (simd_store(F { }, std::get<N>(iterators), std::get<N>(data)), ...);
+            (increment_simd_iterator<F>(&std::get<N>(iterators)), ...);
+        }
+
+        std::tuple<I...> iterators;
+    };
+
+    // A SIMD pipeline is composed of one or more functor objects, each of
+    // which defines an operator() with the following signature:
+    //  template<simd flags> auto operator()(simd_format fmt, auto... src)
+    // The input data is wrapped by simd_data() so that it encodes the type of
+    // data that is being operated on.  This can be recovered via simd_type.
+    // Data is then returned via simd_return(fmt, simd_data<T>(dst)...), which
+    // is passed on to the next stage.
+    template<typename... T>
+    class simd_pipeline
+    {
+        std::tuple<T...> stages;
+
+        template<simd flags, std::size_t stage, simd_format Fmt, typename... A>
+        static consteval bool invocable(std::type_identity<std::tuple<A...>>)
+        {
+            if constexpr (not flags.match(simd_format_traits<Fmt>::flags)) return false;
+            using func = std::tuple_element_t<stage, decltype(stages)>;
+            constexpr bool valid = simd_invocable<func, flags, Fmt, A...>;
+            if constexpr (valid and stage < sizeof...(T) - 1)
+            {
+                using result = simd_invoke_result<func, flags, Fmt, A...>;
+                using format = typename result::format;
+                return invocable<flags, stage + 1, format>(std::type_identity<decltype(result::data)> { });
+            }
+            return valid;
+        }
+
+        template<simd flags, std::size_t stage, simd_format Fmt, typename... A>
+        auto invoke(Fmt, A&&... args)
+        {
+            if constexpr (stage < sizeof...(T) - 1)
+            {
+                auto result = simd_invoke<flags>(std::get<stage>(stages), Fmt { }, std::forward<A>(args)...);
+                using Fmt2 = typename decltype(result)::format;
+                return std::apply([this]<typename... A2>(A2&&... args) { return invoke<flags, stage + 1>(Fmt2 { }, std::forward<A2>(args)...); }, result.data);
+            }
+            else return simd_invoke<flags>(std::get<stage>(stages), Fmt { }, std::forward<A>(args)...);
+        }
+
+    public:
+        template<typename... U>
+        simd_pipeline(U&&... f) : stages { std::forward<U>(f)... } { }
+
+        template<simd flags, simd_format Fmt, typename... A> requires (invocable<flags, 0, Fmt>(std::type_identity<std::tuple<A...>> { }))
+        auto operator()(Fmt, A&&... args)
+        {
+            return invoke<flags, 0>(Fmt { }, std::forward<A>(args)...);
+        }
+    };
+
+    template<typename... T> simd_pipeline(T...) -> simd_pipeline<std::remove_cvref_t<T>...>;
+
+    // Execute a SIMD pipeline or single stage with the specified arguments.
+    template<simd flags, typename F, typename... A>
+    [[gnu::flatten, gnu::hot]] auto simd_run(F&& func, A&&... args)
+    {
         constexpr auto can_invoke = []<typename Fmt>(Fmt) consteval
         {
-            return flags.match(simd_format_traits<Fmt>::flags)
-                and can_load<const From*, Fmt>
-                and can_store<To*, Fmt>
-                and simd_invocable<F, flags, Fmt, decltype(id), typename simd_type_traits<From, Fmt>::data_type, A...>;
+            return flags.match(simd_format_traits<Fmt>::flags) and simd_invocable<F, flags, Fmt, A...>;
         };
 
-        auto do_invoke = [&]<typename Fmt>(Fmt t)
+        auto do_invoke = [&]<typename Fmt>(Fmt)
         {
-            simd_store(t, dst + i, simd_invoke<flags>(func, t, id, simd_load(t, src + i), args...));
-            i += simd_type_traits<From, Fmt>::delta;
+            return simd_invoke<flags>(std::forward<F>(func), Fmt { }, std::forward<A>(args)...);
         };
 
-        static_assert (can_invoke(nosimd));
-
-        while (i < n)
-        {
-            if constexpr (can_invoke(pi8)) do_invoke(pi8);
-            else if constexpr (can_invoke(pi16)) do_invoke(pi16);
-            else if constexpr (can_invoke(pi32)) do_invoke(pi32);
-            else if constexpr (can_invoke(si64)) do_invoke(si64);
-            else if constexpr (can_invoke(ps)) do_invoke(ps);
-            else if constexpr (can_invoke(pf)) do_invoke(pf);
-            else do_invoke(nosimd);
-        }
-        return dst + i;
+        if constexpr (can_invoke(pi8)) return do_invoke(pi8);
+        else if constexpr (can_invoke(pi16)) return do_invoke(pi16);
+        else if constexpr (can_invoke(pi32)) return do_invoke(pi32);
+        else if constexpr (can_invoke(si64)) return do_invoke(si64);
+        else if constexpr (can_invoke(ps)) return do_invoke(ps);
+        else if constexpr (can_invoke(pf)) return do_invoke(pf);
+        else return do_invoke(nosimd);
     }
 }
