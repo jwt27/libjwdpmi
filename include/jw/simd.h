b41d001ff7eeb6ae6004b34c68ab16350241d003
J.W. Jagersma <jwjagersma@gmail.com>
2022-10-31 18:43:23 +0100

    use iterators in simd_load/store

--- include/jw/simd.h
+++ include/jw/simd.h
@@ -170,133 +170,134 @@ namespace jw
         *dst = src;
     }
 
-    template<simd, std::integral T> requires (sizeof(T) == 1)
-    [[gnu::always_inline]] inline __m64 simd_load(format_pi8, const T* src)
+    template<simd, std::contiguous_iterator I> requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) == 1)
+    [[gnu::always_inline]] inline __m64 simd_load(format_pi8, I src)
     {
-        return *reinterpret_cast<const __m64*>(src);
+        return *reinterpret_cast<const __m64*>(&*src);
     }
 
-    template<simd, std::integral T> requires (sizeof(T) == 1)
-    [[gnu::always_inline]] inline void simd_store(format_pi8, T* dst, __m64 src)
+    template<simd, std::contiguous_iterator I> requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) == 1)
+    [[gnu::always_inline]] inline void simd_store(format_pi8, I dst, __m64 src)
     {
-        *reinterpret_cast<__m64*>(dst) = src;
+        *reinterpret_cast<__m64*>(&*dst) = src;
     }
 
-    template<simd flags, std::integral T> requires (sizeof(T) <= 2)
-    [[gnu::always_inline]] inline __m64 simd_load(format_pi16, const T* src)
+    template<simd flags, std::contiguous_iterator I> requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) <= 2)
+    [[gnu::always_inline]] inline __m64 simd_load(format_pi16, const I src)
     {
-        if constexpr (sizeof(T) == 2) return *reinterpret_cast<const __m64*>(src);
+        if constexpr (sizeof(std::iter_value_t<I>) == 2) return *reinterpret_cast<const __m64*>(&*src);
         else
         {
             __m64 data = simd_load<flags>(pi8, src);
             __m64 sign = _mm_setzero_si64();
-            if constexpr (std::is_signed_v<T>) sign = _mm_cmpgt_pi8(sign, data);
+            if constexpr (std::is_signed_v<std::iter_value_t<I>>) sign = _mm_cmpgt_pi8(sign, data);
             data = _mm_unpacklo_pi8(data, sign);
             return data;
         }
     }
 
-    template<simd, std::integral T> requires (sizeof(T) == 2)
-    [[gnu::always_inline]] inline void simd_store(format_pi16, T* dst, __m64 src)
+    template<simd, std::contiguous_iterator I> requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) == 2)
+    [[gnu::always_inline]] inline void simd_store(format_pi16, I dst, __m64 src)
     {
-        *reinterpret_cast<__m64*>(dst) = src;
+        *reinterpret_cast<__m64*>(&*dst) = src;
     }
 
-    template<simd, std::integral T> requires (sizeof(T) == 1)
-    [[gnu::always_inline]] inline void simd_store(format_pi16, T* dst, __m64 src)
+    template<simd, std::contiguous_iterator I> requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) == 1)
+    [[gnu::always_inline]] inline void simd_store(format_pi16, I dst, __m64 src)
     {
-        const __m64 a = std::is_signed_v<T> ? _mm_packs_pi16(src, src) : _mm_packs_pu16(src, src);
-        *reinterpret_cast<std::uint32_t*>(dst) = _mm_cvtsi64_si32(a);
+        const __m64 a = std::is_signed_v<std::iter_value_t<I>> ? _mm_packs_pi16(src, src) : _mm_packs_pu16(src, src);
+        *reinterpret_cast<std::uint32_t*>(&*dst) = _mm_cvtsi64_si32(a);
     }
 
-    template<simd flags, std::integral T> requires (sizeof(T) <= 4)
-    [[gnu::always_inline]] inline __m64 simd_load(format_pi32, const T* src)
+    template<simd flags, std::contiguous_iterator I> requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) <= 4)
+    [[gnu::always_inline]] inline __m64 simd_load(format_pi32, const I src)
     {
-        if constexpr (sizeof(T) == 4) return *reinterpret_cast<const __m64*>(src);
+        if constexpr (sizeof(std::iter_value_t<I>) == 4) return *reinterpret_cast<const __m64*>(&*src);
         else
         {
             __m64 data = simd_load<flags>(pi16, src);
             __m64 sign = _mm_setzero_si64();
-            if constexpr (std::is_signed_v<T>) sign = _mm_cmpgt_pi16(sign, data);
+            if constexpr (std::is_signed_v<std::iter_value_t<I>>) sign = _mm_cmpgt_pi16(sign, data);
             data = _mm_unpacklo_pi16(data, sign);
             return data;
         }
     }
 
-    template<simd, std::integral T> requires (sizeof(T) == 4)
-    [[gnu::always_inline]] inline void simd_store(format_pi32, T* dst, __m64 src)
+    template<simd, std::contiguous_iterator I> requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) == 4)
+    [[gnu::always_inline]] inline void simd_store(format_pi32, I dst, __m64 src)
     {
-        *reinterpret_cast<__m64*>(dst) = src;
+        *reinterpret_cast<__m64*>(&*dst) = src;
     }
 
-    template<simd, std::signed_integral T> requires (sizeof(T) == 2)
-    [[gnu::always_inline]] inline void simd_store(format_pi32, T* dst, __m64 src)
+    template<simd, std::contiguous_iterator I> requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) == 2)
+    [[gnu::always_inline]] inline void simd_store(format_pi32, I dst, __m64 src)
     {
         const __m64 a = _mm_packs_pi32(src, src);
-        *reinterpret_cast<std::uint32_t*>(dst) = _mm_cvtsi64_si32(a);
+        *reinterpret_cast<std::uint32_t*>(&*dst) = _mm_cvtsi64_si32(a);
     }
 
-    template<simd, std::integral T> requires (sizeof(T) == 1)
-    [[gnu::always_inline]] inline void simd_store(format_pi32, T* dst, __m64 src)
+    template<simd, std::contiguous_iterator I> requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) == 1)
+    [[gnu::always_inline]] inline void simd_store(format_pi32, I dst, __m64 src)
     {
         __m64 data = _mm_packs_pi32(src, src);
-        data = std::is_signed_v<T> ? _mm_packs_pi16(data, data) : _mm_packs_pu16(data, data);
-        *reinterpret_cast<std::uint16_t*>(dst) = _mm_cvtsi64_si32(data);
+        data = std::is_signed_v<std::iter_value_t<I>> ? _mm_packs_pi16(data, data) : _mm_packs_pu16(data, data);
+        *reinterpret_cast<std::uint16_t*>(&*dst) = _mm_cvtsi64_si32(data);
     }
 
-    template<simd flags, std::integral T> requires (sizeof(T) <= 8)
-    [[gnu::always_inline]] inline __m64 simd_load(format_si64, const T* src)
+    template<simd flags, std::contiguous_iterator I> requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) <= 8)
+    [[gnu::always_inline]] inline __m64 simd_load(format_si64, const I src)
     {
-        if constexpr (sizeof(T) == 8) return *reinterpret_cast<const __m64*>(src);
+        if constexpr (sizeof(std::iter_value_t<I>) == 8) return *reinterpret_cast<const __m64*>(&*src);
         else
         {
             __m64 data = simd_load<flags>(pi32, src);
             __m64 sign = _mm_setzero_si64();
-            if constexpr (std::is_signed_v<T>) sign = _mm_cmpgt_pi32(sign, data);
+            if constexpr (std::is_signed_v<std::iter_value_t<I>>) sign = _mm_cmpgt_pi32(sign, data);
             data = _mm_unpacklo_pi32(data, sign);
             return data;
         }
     }
 
-    template<simd, std::integral T> requires (sizeof(T) == 8)
-    [[gnu::always_inline]] inline void simd_store(format_si64, T* dst, __m64 src)
+    template<simd, typename I> requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) == 8)
+    [[gnu::always_inline]] inline void simd_store(format_si64, I dst, __m64 src)
     {
-        *reinterpret_cast<__m64*>(dst) = src;
+        *reinterpret_cast<__m64*>(&*dst) = src;
     }
 
-    template<simd>
-    [[gnu::always_inline]] inline __m128 simd_load(format_ps, const float* src)
+    template<simd, std::contiguous_iterator I> requires (std::same_as<std::iter_value_t<I>, float>)
+    [[gnu::always_inline]] inline __m128 simd_load(format_ps, I src)
     {
-        return *reinterpret_cast<const __m128*>(src);
+        return *reinterpret_cast<const __m128*>(&*src);
     }
 
-    template<simd, std::signed_integral T> requires (sizeof(T) == 4)
-    [[gnu::always_inline]] inline __m128 simd_load(format_ps, const T* src)
+    template<simd, std::contiguous_iterator I> requires (std::signed_integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) == 4)
+    [[gnu::always_inline]] inline __m128 simd_load(format_ps, I src)
     {
-        const __m64 lo = *reinterpret_cast<const __m64*>(src + 0);
-        const __m64 hi = *reinterpret_cast<const __m64*>(src + 2);
+        const __m64 lo = *reinterpret_cast<const __m64*>(&*(src + 0));
+        const __m64 hi = *reinterpret_cast<const __m64*>(&*(src + 2));
         return _mm_cvtpi32x2_ps(lo, hi);
     }
 
-    template<simd, std::integral T> requires (sizeof(T) <= 2)
-    [[gnu::always_inline]] inline __m128 simd_load(format_ps, const T* src)
+    template<simd, std::contiguous_iterator I> requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) <= 2)
+    [[gnu::always_inline]] inline __m128 simd_load(format_ps, I src)
     {
         const __m64 data = simd_load(pi16, src);
-        return std::is_signed_v<T> ? _mm_cvtpi16_ps(data) : _mm_cvtpu16_ps(data);
+        return std::is_signed_v<std::iter_value_t<I>> ? _mm_cvtpi16_ps(data) : _mm_cvtpu16_ps(data);
     }
 
-    template<simd>
-    [[gnu::always_inline]] inline void simd_store(format_ps, float* dst, __m128 src)
+    template<simd, std::contiguous_iterator I> requires (std::same_as<std::iter_value_t<I>, float>)
+    [[gnu::always_inline]] inline void simd_store(format_ps, I dst, __m128 src)
     {
-        *reinterpret_cast<__m128*>(dst) = src;
+        *reinterpret_cast<__m128*>(&*dst) = src;
     }
 
-    template<simd flags, std::integral T> requires (sizeof(T) <= 4 and (std::is_signed_v<T> or sizeof(T) == 1))
-    [[gnu::always_inline]] inline void simd_store(format_ps, T* dst, __m128 src)
+    template<simd flags, std::contiguous_iterator I>
+    requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) <= 4 and (std::is_signed_v<std::iter_value_t<I>> or sizeof(std::iter_value_t<I>) == 1))
+    [[gnu::always_inline]] inline void simd_store(format_ps, I dst, __m128 src)
     {
         const __m64 lo = _mm_cvtps_pi32(src);
         const __m64 hi = _mm_cvtps_pi32(_mm_movehl_ps(src, src));
-        if constexpr (simd_storable<T*, flags, format_pi16>)
+        if constexpr (simd_storable<I, flags, format_pi16>)
             simd_store(pi16, dst, _mm_packs_pi32(lo, hi));
         else
         {
@@ -305,26 +306,28 @@ namespace jw
         }
     }
 
-    template<simd>
-    [[gnu::always_inline]] inline __m64 simd_load(format_pf, const float* src)
+    template<simd, std::contiguous_iterator I> requires (std::same_as<std::iter_value_t<I>, float>)
+    [[gnu::always_inline]] inline __m64 simd_load(format_pf, I src)
     {
-        return *reinterpret_cast<const __m64*>(src);
+        return *reinterpret_cast<const __m64*>(&*src);
     }
 
-    template<simd, std::integral T> requires (sizeof(T) <= 4 and (std::is_signed_v<T> or sizeof(T) <= 2))
-    [[gnu::always_inline]] inline __m64 simd_load(format_pf, const T* src)
+    template<simd, std::contiguous_iterator I>
+    requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) <= 4 and (std::is_signed_v<std::iter_value_t<I>> or sizeof(std::iter_value_t<I>) <= 2))
+    [[gnu::always_inline]] inline __m64 simd_load(format_pf, const I src)
     {
         return _m_pi2fd(simd_load(pi32, src));
     }
 
-    template<simd>
-    [[gnu::always_inline]] inline void simd_store(format_pf, float* dst, __m64 src)
+    template<simd, std::contiguous_iterator I> requires (std::same_as<std::iter_value_t<I>, float>)
+    [[gnu::always_inline]] inline void simd_store(format_pf, I dst, __m64 src)
     {
-        *reinterpret_cast<__m64*>(dst) = src;
+        *reinterpret_cast<__m64*>(&*dst) = src;
     }
 
-    template<simd, std::integral T> requires (sizeof(T) <= 4 and (std::is_signed_v<T> or sizeof(T) == 1))
-    [[gnu::always_inline]] inline void simd_store(format_pf, T* dst, __m64 src)
+    template<simd, std::contiguous_iterator I>
+    requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) <= 4 and (std::is_signed_v<std::iter_value_t<I>> or sizeof(std::iter_value_t<I>) == 1))
+    [[gnu::always_inline]] inline void simd_store(format_pf, I dst, __m64 src)
     {
         simd_store(pi32, dst, _m_pf2id(src));
     }
