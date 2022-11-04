53cf655a036b0bc5cd21dab297867eb8603c2da3
J.W. Jagersma <jwjagersma@gmail.com>
2022-11-04 16:33:02 +0100

    add some missing simd template arguments

--- include/jw/simd.h
+++ include/jw/simd.h
@@ -278,10 +278,10 @@ namespace jw
         return _mm_cvtpi32x2_ps(lo, hi);
     }
 
-    template<simd, std::contiguous_iterator I> requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) <= 2)
+    template<simd flags, std::contiguous_iterator I> requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) <= 2)
     [[gnu::always_inline]] inline __m128 simd_load(format_ps, I src)
     {
-        const __m64 data = simd_load(pi16, src);
+        const __m64 data = simd_load<flags>(pi16, src);
         return std::is_signed_v<std::iter_value_t<I>> ? _mm_cvtpi16_ps(data) : _mm_cvtpu16_ps(data);
     }
 
@@ -298,11 +298,11 @@ namespace jw
         const __m64 lo = _mm_cvtps_pi32(src);
         const __m64 hi = _mm_cvtps_pi32(_mm_movehl_ps(src, src));
         if constexpr (simd_storable<I, flags, format_pi16>)
-            simd_store(pi16, dst, _mm_packs_pi32(lo, hi));
+            simd_store<flags>(pi16, dst, _mm_packs_pi32(lo, hi));
         else
         {
-            simd_store(pi32, dst + 0, lo);
-            simd_store(pi32, dst + 2, hi);
+            simd_store<flags>(pi32, dst + 0, lo);
+            simd_store<flags>(pi32, dst + 2, hi);
         }
     }
 
@@ -312,11 +312,11 @@ namespace jw
         return *reinterpret_cast<const __m64*>(&*src);
     }
 
-    template<simd, std::contiguous_iterator I>
+    template<simd flags, std::contiguous_iterator I>
     requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) <= 4 and (std::is_signed_v<std::iter_value_t<I>> or sizeof(std::iter_value_t<I>) <= 2))
     [[gnu::always_inline]] inline __m64 simd_load(format_pf, const I src)
     {
-        return _m_pi2fd(simd_load(pi32, src));
+        return _m_pi2fd(simd_load<flags>(pi32, src));
     }
 
     template<simd, std::contiguous_iterator I> requires (std::same_as<std::iter_value_t<I>, float>)
@@ -325,11 +325,11 @@ namespace jw
         *reinterpret_cast<__m64*>(&*dst) = src;
     }
 
-    template<simd, std::contiguous_iterator I>
+    template<simd flags, std::contiguous_iterator I>
     requires (std::integral<std::iter_value_t<I>> and sizeof(std::iter_value_t<I>) <= 4 and (std::is_signed_v<std::iter_value_t<I>> or sizeof(std::iter_value_t<I>) == 1))
     [[gnu::always_inline]] inline void simd_store(format_pf, I dst, __m64 src)
     {
-        simd_store(pi32, dst, _m_pf2id(src));
+        simd_store<flags>(pi32, dst, _m_pf2id(src));
     }
 
     template<typename F, simd flags, typename... A>
