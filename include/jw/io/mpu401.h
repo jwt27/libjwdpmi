4e3f0f60fe5edc863d75906ea888351c5d18de13
J.W. Jagersma <jwjagersma@gmail.com>
2023-05-31 18:40:13 +0200

    use nested namespace specifiers in mpu401 code

--- include/jw/io/mpu401.h
+++ include/jw/io/mpu401.h
@@ -1,4 +1,5 @@
 /* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
+/* Copyright (C) 2023 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2021 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2020 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2019 J.W. Jagersma, see COPYING.txt for details */
@@ -7,128 +8,125 @@
 
 #pragma once
 #include <iostream>
-#include <mutex>
 #include <unordered_set>
 #include <jw/io/ioport.h>
 #include <jw/dpmi/irq_handler.h>
 #include <jw/common.h>
 #include <jw/io/io_error.h>
-#include <jw/mutex.h>
 #include <jw/io/realtime_streambuf.h>
 
-namespace jw
+namespace jw::io
 {
-    namespace io
+    struct mpu401_config
     {
-        struct mpu401_config
+        port_num port { 0x330 };
+        dpmi::irq_level irq { 9 };
+        bool use_irq { true };
+    };
+}
+namespace jw::io::detail
+{
+    struct [[gnu::packed]] mpu401_status
+    {
+        unsigned : 6;
+        bool dont_send_data : 1;
+        bool no_data_available : 1;
+    };
+
+    struct mpu401_streambuf final : realtime_streambuf
+    {
+        mpu401_streambuf(const mpu401_config& c);
+        virtual ~mpu401_streambuf();
+
+        mpu401_streambuf(const mpu401_streambuf&) = delete;
+        mpu401_streambuf(mpu401_streambuf&&) = delete;
+        mpu401_streambuf& operator=(const mpu401_streambuf&) = delete;
+        mpu401_streambuf& operator=(mpu401_streambuf&&) = delete;
+
+        virtual void put_realtime(char_type out) override;
+
+    protected:
+        virtual int sync() override;
+        virtual std::streamsize xsgetn(char_type* s, std::streamsize n) override;
+        virtual int_type underflow() override;
+        virtual std::streamsize xsputn(const char_type* s, std::streamsize n) override;
+        virtual int_type overflow(int_type c = traits_type::eof()) override;
+
+    private:
+        void check_irq_exception();
+
+        mpu401_status get()
         {
-            port_num port { 0x330 };
-            dpmi::irq_level irq { 9 };
-            bool use_irq { true };
-        };
-
-        namespace detail
-        {
-            struct [[gnu::packed]] mpu401_status
+            const auto qsize = rx_buf.size() / 4;
+            const auto offset = gptr() - (rx_buf.begin() + qsize);
+            if (rx_ptr > rx_buf.begin() + qsize * 3 and offset > 0) [[unlikely]]
             {
-                unsigned : 6;
-                bool dont_send_data : 1;
-                bool no_data_available : 1;
-            };
-
-            struct mpu401_streambuf final : realtime_streambuf
-            {
-                mpu401_streambuf(const mpu401_config& c);
-                virtual ~mpu401_streambuf();
-
-                mpu401_streambuf(const mpu401_streambuf&) = delete;
-                mpu401_streambuf(mpu401_streambuf&&) = delete;
-                mpu401_streambuf& operator=(const mpu401_streambuf&) = delete;
-                mpu401_streambuf& operator=(mpu401_streambuf&&) = delete;
-
-                virtual void put_realtime(char_type out) override;
-
-            protected:
-                virtual int sync() override;
-                virtual std::streamsize xsgetn(char_type* s, std::streamsize n) override;
-                virtual int_type underflow() override;
-                virtual std::streamsize xsputn(const char_type* s, std::streamsize n) override;
-                virtual int_type overflow(int_type c = traits_type::eof()) override;
-
-            private:
-                void check_irq_exception();
-
-                mpu401_status get()
-                {
-                    const auto qsize = rx_buf.size() / 4;
-                    const auto offset = gptr() - (rx_buf.begin() + qsize);
-                    if (rx_ptr > rx_buf.begin() + qsize * 3 and offset > 0) [[unlikely]]
-                    {
-                        std::copy(rx_buf.begin() + offset, rx_ptr, rx_buf.begin());
-                        rx_ptr -= offset;
-                        setg(rx_buf.begin(), rx_buf.begin() + qsize, rx_ptr);
-                    }
-
-                    auto status = status_port.read();
-                    while (not status.no_data_available)
-                    {
-                        if (rx_ptr == rx_buf.end()) throw io::overflow { "MPU401 receive buffer overflow" };
-                        *rx_ptr++ = data_port.read();
-                        status = status_port.read();
-                    }
-                    setg(rx_buf.begin(), gptr(), rx_ptr);
-                    return status;
-                }
-
-                void do_sync()
-                {
-                    while (true)
-                    {
-                        auto status = get();
-                        if (status.dont_send_data or tx_ptr == pptr()) break;
-                        data_port.write(*tx_ptr++);
-                    }
-                }
-
-                dpmi::irq_handler irq_handler { [this]()
-                {
-                    dpmi::interrupt_mask no_irq { };
-                    try
-                    {
-                        if (not status_port.read().no_data_available)
-                            dpmi::irq_handler::acknowledge();
-
-                        do { do_sync(); } while (not status_port.read().no_data_available);
-                    }
-                    catch (...) { irq_exception = std::current_exception(); }
-                } };
-
-                mpu401_config cfg;
-                out_port<byte> cmd_port;
-                in_port<mpu401_status> status_port;
-                io_port<byte> data_port;
-                std::exception_ptr irq_exception;
-
-                std::array<char_type, 1_KB> rx_buf;
-                std::array<char_type, 1_KB> tx_buf;
-                char_type* rx_ptr { rx_buf.data() };
-                char_type* tx_ptr { tx_buf.data() };
-
-                inline static std::unordered_set<port_num> ports_used { };
-            };
-        }
-
-        struct mpu401_stream : std::iostream
-        {
-            mpu401_stream(mpu401_config c = { }) : std::iostream(nullptr), streambuf(new detail::mpu401_streambuf { c })
-            {
-                this->rdbuf(streambuf.get());
+                std::copy(rx_buf.begin() + offset, rx_ptr, rx_buf.begin());
+                rx_ptr -= offset;
+                setg(rx_buf.begin(), rx_buf.begin() + qsize, rx_ptr);
             }
 
-            mpu401_stream(const mpu401_stream&) = delete;
+            auto status = status_port.read();
+            while (not status.no_data_available)
+            {
+                if (rx_ptr == rx_buf.end()) throw io::overflow { "MPU401 receive buffer overflow" };
+                *rx_ptr++ = data_port.read();
+                status = status_port.read();
+            }
+            setg(rx_buf.begin(), gptr(), rx_ptr);
+            return status;
+        }
 
-        private:
-            std::unique_ptr<detail::mpu401_streambuf> streambuf;
-        };
-    }
+        void do_sync()
+        {
+            while (true)
+            {
+                auto status = get();
+                if (status.dont_send_data or tx_ptr == pptr()) break;
+                data_port.write(*tx_ptr++);
+            }
+        }
+
+        dpmi::irq_handler irq_handler { [this]()
+        {
+            dpmi::interrupt_mask no_irq { };
+            try
+            {
+                if (not status_port.read().no_data_available)
+                    dpmi::irq_handler::acknowledge();
+
+                do { do_sync(); } while (not status_port.read().no_data_available);
+            }
+            catch (...) { irq_exception = std::current_exception(); }
+        } };
+
+        mpu401_config cfg;
+        out_port<byte> cmd_port;
+        in_port<mpu401_status> status_port;
+        io_port<byte> data_port;
+        std::exception_ptr irq_exception;
+
+        std::array<char_type, 1_KB> rx_buf;
+        std::array<char_type, 1_KB> tx_buf;
+        char_type* rx_ptr { rx_buf.data() };
+        char_type* tx_ptr { tx_buf.data() };
+
+        inline static std::unordered_set<port_num> ports_used { };
+    };
+}
+
+namespace jw::io
+{
+    struct mpu401_stream : std::iostream
+    {
+        mpu401_stream(mpu401_config c = { }) : std::iostream(nullptr), streambuf(new detail::mpu401_streambuf { c })
+        {
+            this->rdbuf(streambuf.get());
+        }
+
+        mpu401_stream(const mpu401_stream&) = delete;
+
+    private:
+        std::unique_ptr<detail::mpu401_streambuf> streambuf;
+    };
 }
--- src/mpu401.cpp
+++ src/mpu401.cpp
@@ -1,4 +1,5 @@
 /* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
+/* Copyright (C) 2023 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2021 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2020 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2019 J.W. Jagersma, see COPYING.txt for details */
@@ -7,217 +8,212 @@
 
 #include <jw/io/mpu401.h>
 #include <jw/chrono.h>
+#include <jw/thread.h>
 
-namespace jw
+namespace jw::io::detail
 {
-    namespace io
+    mpu401_streambuf::mpu401_streambuf(const mpu401_config& c) : cfg(c), cmd_port(cfg.port + 1), status_port(cfg.port + 1), data_port(cfg.port)
     {
-        namespace detail
+        if (ports_used.contains(cfg.port)) throw std::runtime_error("MPU401 port already in use.");
+        ports_used.insert(cfg.port);
+
+        try
         {
-            mpu401_streambuf::mpu401_streambuf(const mpu401_config& c) : cfg(c), cmd_port(cfg.port + 1), status_port(cfg.port + 1), data_port(cfg.port)
+            using namespace std::chrono_literals;
+
+            auto fail = [] { throw device_not_found { "MPU401 not detected" }; };
+
+            std::optional<dpmi::irq_mask> no_irq;
+            if (cfg.use_irq) no_irq.emplace(cfg.irq);
+
+            auto timeout = this_thread::yield_while_for([this]
             {
-                if (ports_used.contains(cfg.port)) throw std::runtime_error("MPU401 port already in use.");
-                ports_used.insert(cfg.port);
+                while (not status_port.read().no_data_available) data_port.read();
+                return status_port.read().dont_send_data;
+            }, 100ms);
+            if (timeout) fail();
 
-                try
+            cmd_port.write(0xff);   // Reset (this won't ACK if the MPU is in uart mode already)
+
+            timeout = this_thread::yield_while_for([this, fail]
+            {
+                const auto s = status_port.read();
+                if (not s.no_data_available and data_port.read() != 0xfe) fail();
+                return s.dont_send_data;
+            }, 100ms);
+            if (timeout) fail();
+
+            cmd_port.write(0x3f);   // Set UART mode (should ACK, maybe some cheap cards won't)
+
+            timeout = this_thread::yield_while_for([this] { return status_port.read().no_data_available; }, 250ms);
+            if (not timeout)
+            {
+                do
                 {
-                    using namespace std::chrono_literals;
-
-                    auto fail = [] { throw device_not_found { "MPU401 not detected" }; };
-
-                    std::optional<dpmi::irq_mask> no_irq;
-                    if (cfg.use_irq) no_irq.emplace(cfg.irq);
-
-                    auto timeout = this_thread::yield_while_for([this]
-                    {
-                        while (not status_port.read().no_data_available) data_port.read();
-                        return status_port.read().dont_send_data;
-                    }, 100ms);
-                    if (timeout) fail();
-
-                    cmd_port.write(0xff);   // Reset (this won't ACK if the MPU is in uart mode already)
-
-                    timeout = this_thread::yield_while_for([this, fail]
-                    {
-                        const auto s = status_port.read();
-                        if (not s.no_data_available and data_port.read() != 0xfe) fail();
-                        return s.dont_send_data;
-                    }, 100ms);
-                    if (timeout) fail();
-
-                    cmd_port.write(0x3f);   // Set UART mode (should ACK, maybe some cheap cards won't)
-
-                    timeout = this_thread::yield_while_for([this] { return status_port.read().no_data_available; }, 250ms);
-                    if (not timeout)
-                    {
-                        do
-                        {
-                            if (data_port.read() != 0xfe) fail();
-                        } while (not status_port.read().no_data_available);
-                    }
-
-                    setg(rx_buf.begin(), rx_buf.begin(), rx_buf.begin());
-                    setp(tx_buf.begin(), tx_buf.end());
-
-                    irq_handler.set_irq(cfg.irq);
-                    if (cfg.use_irq) irq_handler.enable();
-                }
-                catch (...)
-                {
-                    ports_used.erase(cfg.port);
-                    throw;
-                }
+                    if (data_port.read() != 0xfe) fail();
+                } while (not status_port.read().no_data_available);
             }
 
-            mpu401_streambuf::~mpu401_streambuf()
-            {
-                irq_handler.disable();
-                this_thread::yield_while([this]
-                {
-                    auto status = status_port.read();
-                    while (not status.no_data_available)
-                    {
-                        data_port.read();
-                        status = status_port.read();
-                    }
-                    return status.dont_send_data;
-                });
-                cmd_port.write(0xff);
-                ports_used.erase(cfg.port);
-            }
+            setg(rx_buf.begin(), rx_buf.begin(), rx_buf.begin());
+            setp(tx_buf.begin(), tx_buf.end());
 
-            void mpu401_streambuf::put_realtime(char_type out)
+            irq_handler.set_irq(cfg.irq);
+            if (cfg.use_irq) irq_handler.enable();
+        }
+        catch (...)
+        {
+            ports_used.erase(cfg.port);
+            throw;
+        }
+    }
+
+    mpu401_streambuf::~mpu401_streambuf()
+    {
+        irq_handler.disable();
+        this_thread::yield_while([this]
+        {
+            auto status = status_port.read();
+            while (not status.no_data_available)
             {
-                while (true)
+                data_port.read();
+                status = status_port.read();
+            }
+            return status.dont_send_data;
+        });
+        cmd_port.write(0xff);
+        ports_used.erase(cfg.port);
+    }
+
+    void mpu401_streambuf::put_realtime(char_type out)
+    {
+        while (true)
+        {
+            {
+                std::optional<dpmi::interrupt_mask> no_irq;
+                if (cfg.use_irq) no_irq.emplace();
+                auto status = get();
+                if (not status.dont_send_data)
                 {
-                    {
-                        std::optional<dpmi::interrupt_mask> no_irq;
-                        if (cfg.use_irq) no_irq.emplace();
-                        auto status = get();
-                        if (not status.dont_send_data)
-                        {
-                            data_port.write(out);
-                            return;
-                        }
-                    }
-                    this_thread::yield();
+                    data_port.write(out);
+                    return;
                 }
             }
+            this_thread::yield();
+        }
+    }
 
-            int mpu401_streambuf::sync()
+    int mpu401_streambuf::sync()
+    {
+        if (cfg.use_irq)
+        {
+            this_thread::yield_while([this]
             {
-                if (cfg.use_irq)
+                check_irq_exception();
                 {
-                    this_thread::yield_while([this]
-                    {
-                        check_irq_exception();
-                        {
-                            dpmi::interrupt_mask no_irq { };
-                            do_sync();
-                        }
-                        return tx_ptr < pptr();
-                    });
+                    dpmi::interrupt_mask no_irq { };
+                    do_sync();
                 }
-                else
-                {
-                    this_thread::yield_while([this]
-                    {
-                        do_sync();
-                        return tx_ptr < pptr();
-                    });
-                }
-                setp(tx_buf.begin(), tx_buf.end());
-                tx_ptr = tx_buf.begin();
-                return 0;
-            }
-
-            std::streamsize mpu401_streambuf::xsgetn(char_type* s, std::streamsize max)
+                return tx_ptr < pptr();
+            });
+        }
+        else
+        {
+            this_thread::yield_while([this]
             {
-                std::streamsize size = 0;
-                while (size < max)
-                {
-                    auto n = std::min(egptr() - gptr(), max - size);
-                    std::copy_n(gptr(), n, s);
-                    gbump(n);
-                    s += n;
-                    size += n;
-                    if (size < max) underflow();
-                }
-                return size;
-            }
+                do_sync();
+                return tx_ptr < pptr();
+            });
+        }
+        setp(tx_buf.begin(), tx_buf.end());
+        tx_ptr = tx_buf.begin();
+        return 0;
+    }
 
-            mpu401_streambuf::int_type mpu401_streambuf::underflow()
+    std::streamsize mpu401_streambuf::xsgetn(char_type* s, std::streamsize max)
+    {
+        std::streamsize size = 0;
+        while (size < max)
+        {
+            auto n = std::min(egptr() - gptr(), max - size);
+            std::copy_n(gptr(), n, s);
+            gbump(n);
+            s += n;
+            size += n;
+            if (size < max) underflow();
+        }
+        return size;
+    }
+
+    mpu401_streambuf::int_type mpu401_streambuf::underflow()
+    {
+        this_thread::yield_while([this]
+        {
+            check_irq_exception();
+            {
+                std::optional<dpmi::interrupt_mask> no_irq;
+                if (cfg.use_irq) no_irq.emplace();
+                get();
+            }
+            return gptr() == rx_ptr;
+        });
+        return *gptr();
+    }
+
+    std::streamsize mpu401_streambuf::xsputn(const char_type* s, std::streamsize max)
+    {
+        std::streamsize size = 0;
+        while (size < max)
+        {
+            auto n = std::min(epptr() - pptr(), max - size);
+            std::copy_n(s, n, pptr());
+            pbump(n);
+            s += n;
+            size += n;
+            if (size < max) overflow();
+        }
+        return size;
+    }
+
+    mpu401_streambuf::int_type mpu401_streambuf::overflow(int_type c)
+    {
+        if (pptr() == epptr())
+        {
+            if (cfg.use_irq)
             {
                 this_thread::yield_while([this]
                 {
                     check_irq_exception();
                     {
-                        std::optional<dpmi::interrupt_mask> no_irq;
-                        if (cfg.use_irq) no_irq.emplace();
-                        get();
+                        dpmi::interrupt_mask no_irq { };
+                        do_sync();
                     }
-                    return gptr() == rx_ptr;
+                    return tx_ptr == pbase();
                 });
-                return *gptr();
             }
-
-            std::streamsize mpu401_streambuf::xsputn(const char_type* s, std::streamsize max)
+            else
             {
-                std::streamsize size = 0;
-                while (size < max)
+                this_thread::yield_while([this]
                 {
-                    auto n = std::min(epptr() - pptr(), max - size);
-                    std::copy_n(s, n, pptr());
-                    pbump(n);
-                    s += n;
-                    size += n;
-                    if (size < max) overflow();
-                }
-                return size;
+                    do_sync();
+                    return tx_ptr == pbase();
+                });
             }
+            std::copy(tx_ptr, pptr(), pbase());
+            pbump(pbase() - tx_ptr);
+            tx_ptr = pbase();
+        }
 
-            mpu401_streambuf::int_type mpu401_streambuf::overflow(int_type c)
-            {
-                if (pptr() == epptr())
-                {
-                    if (cfg.use_irq)
-                    {
-                        this_thread::yield_while([this]
-                        {
-                            check_irq_exception();
-                            {
-                                dpmi::interrupt_mask no_irq { };
-                                do_sync();
-                            }
-                            return tx_ptr == pbase();
-                        });
-                    }
-                    else
-                    {
-                        this_thread::yield_while([this]
-                        {
-                            do_sync();
-                            return tx_ptr == pbase();
-                        });
-                    }
-                    std::copy(tx_ptr, pptr(), pbase());
-                    pbump(pbase() - tx_ptr);
-                    tx_ptr = pbase();
-                }
+        if (traits_type::not_eof(c)) sputc(c);
+        return ~traits_type::eof();
+    }
 
-                if (traits_type::not_eof(c)) sputc(c);
-                return ~traits_type::eof();
-            }
-
-            void mpu401_streambuf::check_irq_exception()
-            {
-                if (irq_exception != nullptr) [[unlikely]]
-                {
-                    std::exception_ptr e { nullptr };
-                    std::swap(e, irq_exception);
-                    std::rethrow_exception(e);
-                }
-            }
+    void mpu401_streambuf::check_irq_exception()
+    {
+        if (irq_exception != nullptr) [[unlikely]]
+        {
+            std::exception_ptr e { nullptr };
+            std::swap(e, irq_exception);
+            std::rethrow_exception(e);
         }
     }
 }
