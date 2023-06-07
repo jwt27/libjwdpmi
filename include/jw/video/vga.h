eacefff752c53611c19aefc26b4e3d548a4fd72b
J.W. Jagersma <jwjagersma@gmail.com>
2023-06-07 23:18:40 +0200

    use nested namespace specifiers in vga code

--- include/jw/video/vga.h
+++ include/jw/video/vga.h
@@ -1,4 +1,5 @@
 /* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
+/* Copyright (C) 2023 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2022 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2018 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2017 J.W. Jagersma, see COPYING.txt for details */
@@ -10,28 +11,20 @@
 #include <jw/video/pixel.h>
 #include <jw/io/ioport.h>
 
-namespace jw
+namespace jw::video
 {
-    namespace video
+    struct vga_bios
     {
-        struct vga_bios
-        {
-            virtual void set_mode(vbe_mode m, const crtc_info* = nullptr);
-        };
+        virtual void set_mode(vbe_mode m, const crtc_info* = nullptr);
+    };
 
-        struct vga : public vga_bios
-        {
-            virtual void set_palette(std::span<const px32n>, std::size_t first = 0, bool wait_for_vsync = false);
+    struct vga : public vga_bios
+    {
+        virtual void set_palette(std::span<const px32n>, std::size_t first = 0, bool wait_for_vsync = false);
 
-            virtual std::array<px32n, 256> get_palette();
+        virtual std::array<px32n, 256> get_palette();
 
-        protected:
-            static inline constexpr io::in_port<bool> dac_state { 0x3c7 };
-            static inline constexpr io::io_port<byte> dac_write_index { 0x3c8 };
-            static inline constexpr io::out_port<byte> dac_read_index { 0x3c7 };
-            static inline constexpr io::io_port<byte> dac_data { 0x3c9 };
-
-            std::size_t dac_bits { 6 };
-        };
-    }
+    protected:
+        std::size_t dac_bits { 6 };
+    };
 }
--- src/vga.cpp
+++ src/vga.cpp
@@ -7,75 +7,77 @@
 #include <jw/video/vga.h>
 #include <jw/dpmi/realmode.h>
 
-namespace jw
+namespace jw::video
 {
-    namespace video
-    {
-        void vga_bios::set_mode(vbe_mode m, const crtc_info *)
-        {
-            if (m.dont_clear_video_memory) m.index |= 0x80;
-            dpmi::realmode_registers reg { };
-            reg.ah = 0x00;
-            reg.al = m.index;
-            reg.call_int(0x10);
-        }
+    static constexpr io::in_port<bool> dac_state { 0x3c7 };
+    static constexpr io::io_port<byte> dac_write_index { 0x3c8 };
+    static constexpr io::out_port<byte> dac_read_index { 0x3c7 };
+    static constexpr io::io_port<byte> dac_data { 0x3c9 };
 
-        void vga::set_palette(std::span<const px32n> pal, std::size_t first, bool)
+    void vga_bios::set_mode(vbe_mode m, const crtc_info *)
+    {
+        if (m.dont_clear_video_memory) m.index |= 0x80;
+        dpmi::realmode_registers reg { };
+        reg.ah = 0x00;
+        reg.al = m.index;
+        reg.call_int(0x10);
+    }
+
+    void vga::set_palette(std::span<const px32n> pal, std::size_t first, bool)
+    {
+        dac_write_index.write(first);
+        if (dac_bits == 8)
         {
-            dac_write_index.write(first);
-            if (dac_bits == 8)
+            for (const auto& i : pal)
             {
+                dac_data.write(i.r);
+                dac_data.write(i.g);
+                dac_data.write(i.b);
+            }
+        }
+        else
+        {
+            mmx_function<default_simd()>([pal]<simd flags>()
+            {
+                simd_pipeline pipe { simd_in, px_convert<pxvga>, simd_out };
                 for (const auto& i : pal)
                 {
-                    dac_data.write(i.r);
-                    dac_data.write(i.g);
-                    dac_data.write(i.b);
+                    const auto p = simd_run<flags>(pipe, i);
+                    dac_data.write(p.r);
+                    dac_data.write(p.g);
+                    dac_data.write(p.b);
                 }
-            }
-            else
+            });
+        }
+    }
+
+    std::array<px32n, 256> vga::get_palette()
+    {
+        std::array<px32n, 256> result;
+        dac_read_index.write(0);
+        if (dac_bits == 8)
+        {
+            for (auto i = 0; i < 256; ++i)
             {
-                mmx_function<default_simd()>([pal]<simd flags>()
-                {
-                    simd_pipeline pipe { simd_in, px_convert<pxvga>, simd_out };
-                    for (const auto& i : pal)
-                    {
-                        const auto p = simd_run<flags>(pipe, i);
-                        dac_data.write(p.r);
-                        dac_data.write(p.g);
-                        dac_data.write(p.b);
-                    }
-                });
+                result[i].r = dac_data.read();
+                result[i].g = dac_data.read();
+                result[i].b = dac_data.read();
             }
         }
-
-        std::array<px32n, 256> vga::get_palette()
+        else
         {
-            std::array<px32n, 256> result;
-            dac_read_index.write(0);
-            if (dac_bits == 8)
+            mmx_function<default_simd()>([p = result.data()]<simd flags>()
             {
+                simd_pipeline pipe { simd_in, px_convert<px32n>, simd_out };
                 for (auto i = 0; i < 256; ++i)
                 {
-                    result[i].r = dac_data.read();
-                    result[i].g = dac_data.read();
-                    result[i].b = dac_data.read();
+                    auto r = dac_data.read();
+                    auto g = dac_data.read();
+                    auto b = dac_data.read();
+                    p[i] = simd_run<flags>(pipe, pxvga { r, g, b });
                 }
-            }
-            else
-            {
-                mmx_function<default_simd()>([p = result.data()]<simd flags>()
-                {
-                    simd_pipeline pipe { simd_in, px_convert<px32n>, simd_out };
-                    for (auto i = 0; i < 256; ++i)
-                    {
-                        auto r = dac_data.read();
-                        auto g = dac_data.read();
-                        auto b = dac_data.read();
-                        p[i] = simd_run<flags>(pipe, pxvga { r, g, b });
-                    }
-                });
-            }
-            return result;
+            });
         }
+        return result;
     }
 }
