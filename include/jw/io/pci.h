94aa87169e2462862e95053f88c184c285f50ee7
J.W. Jagersma <jwjagersma@gmail.com>
2023-06-08 19:09:32 +0200

    use nested namespace specifiers in pci code

--- include/jw/io/pci.h
+++ include/jw/io/pci.h
@@ -1,6 +1,5 @@
-/* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
-/* Copyright (C) 2022 J.W. Jagersma, see COPYING.txt for details */
-/* Copyright (C) 2017 J.W. Jagersma, see COPYING.txt for details */
+#/* * * * * * * * * * * * * * * * * * jwdpmi * * * * * * * * * * * * * * * * * */
+#/*    Copyright (C) 2017 - 2023 J.W. Jagersma, see COPYING.txt for details    */
 
 #pragma once
 #include <initializer_list>
@@ -9,170 +8,167 @@
 #include <jw/io/ioport.h>
 #include <jw/common.h>
 
-namespace jw
+namespace jw::io
 {
-    namespace io
+    struct pci_device
     {
-        struct pci_device
+        struct error : public std::runtime_error { using runtime_error::runtime_error; };
+        struct unsupported_function : public error { using error::error; };
+        struct bad_register : public error { using error::error; };
+        struct device_not_found : public error { using error::error; };
+
+    protected:
+        struct device_tag { };
+        struct class_tag { };
+        pci_device(device_tag, std::uint16_t vendor, std::initializer_list<std::uint16_t> devices, std::uint8_t function = 0xff);
+        pci_device(class_tag, std::uint8_t class_code, std::initializer_list<std::uint8_t> subclass_codes, std::uint8_t interface_type);
+        virtual ~pci_device();
+
+        template<typename T>
+        struct pci_register
         {
-            struct error : public std::runtime_error { using runtime_error::runtime_error; };
-            struct unsupported_function : public error { using error::error; };
-            struct bad_register : public error { using error::error; };
-            struct device_not_found : public error { using error::error; };
-
-        protected:
-            struct device_tag { };
-            struct class_tag { };
-            pci_device(device_tag, std::uint16_t vendor, std::initializer_list<std::uint16_t> devices, std::uint8_t function = 0xff);
-            pci_device(class_tag, std::uint8_t class_code, std::initializer_list<std::uint8_t> subclass_codes, std::uint8_t interface_type);
-            virtual ~pci_device();
-
-            template<typename T>
-            struct pci_register
+            static_assert(sizeof(T) == 4, "PCI registers must be 32 bits wide.");
+            constexpr pci_register(const pci_device* device, std::uint8_t register_num) : dev(*device), reg(register_num)
             {
-                static_assert(sizeof(T) == 4, "PCI registers must be 32 bits wide.");
-                constexpr pci_register(const pci_device* device, std::uint8_t register_num) : dev(*device), reg(register_num)
-                {
-                    if (reg % 4 != 0) throw bad_register { "PCI registers must be aligned to a 32-bit boundary." };
-                }
-
-                auto read()
-                {
-                    index.write(get_regnum());
-                    return data.read();
-                }
-
-                void write(const T& value)
-                {
-                    index.write(get_regnum());
-                    data.write(value);
-                }
-
-            private:
-                std::uint32_t get_regnum() const
-                {
-                    union
-                    {
-                        struct
-                        {
-                            unsigned register_num : 8;
-                            unsigned function : 3;
-                            unsigned device : 5;
-                            unsigned bus : 8;
-                            unsigned : 7;
-                            bool enable_config : 1;
-                        };
-                        std::uint32_t value { };
-                    } x;
-                    x.register_num = reg;
-                    x.function = dev.function;
-                    x.device = dev.device;
-                    x.bus = dev.bus;
-                    x.enable_config = true;
-                    return x.value;
-                }
-
-                const pci_device& dev { };
-                const std::uint8_t reg;
-                static constexpr out_port<std::uint32_t> index { 0xcf8 };
-                static constexpr io_port<T> data { 0xcfc };
-            };
-
-            struct [[gnu::packed]] reg_command
-            {
-                bool io_access : 1;
-                bool memory_access : 1;
-                bool bus_master : 1;
-                bool respond_to_special_cycle : 1;
-                bool enable_memory_write_and_invalidate : 1;
-                bool vga_palette_snoop : 1;
-                bool respond_to_parity_error : 1;
-                bool enable_stepping : 1;               // not used since PCI 3.0
-                bool enable_system_error : 1;
-                bool enable_fast_back_to_back : 1;
-                bool disable_interrupt : 1;
-                unsigned : 5;
-            } command;
-
-            struct [[gnu::packed]] reg_status
-            {
-                unsigned : 3;
-                bool interrupt : 1;
-                bool has_capabilities_list : 1;
-                bool is_66mhz_capable : 1;
-                bool user_definable_configuration : 1;   // not used since PCI 2.2
-                bool is_fast_back_to_back_capable : 1;
-                bool master_parity_error : 1;
-                enum { fast, medium, slow } devsel_timing : 2;
-                bool sent_target_abort : 1;
-                bool received_target_abort : 1;
-                bool received_master_abort : 1;
-                bool sent_system_error : 1;
-                bool parity_error : 1;
-            } status;
-
-            struct [[gnu::packed]] reg_id { std::uint16_t vendor, device; };
-            struct [[gnu::packed]] reg_command_and_status { reg_command command; reg_status status; };
-            struct [[gnu::packed]] reg_type { std::uint8_t revision, prog_interface, subclass, class_code; };
-            struct [[gnu::packed]] reg_bus_info { std::uint8_t irq, interrupt_pin, min_grant, max_latency; };
-            struct [[gnu::packed]] reg_misc
-            {
-                std::uint8_t cache_line_size;
-                std::uint8_t latency_timer;
-                struct [[gnu::packed]]
-                {
-                    unsigned header_type : 7;
-                    bool multifunction : 1;
-                };
-                struct [[gnu::packed]]
-                {
-                    unsigned result : 4;
-                    unsigned : 2;
-                    bool start : 1;
-                    bool is_capable : 1;
-                } self_test;
-            };
-
-            pci_register<reg_id> id { this, 0x00 };
-            pci_register<reg_command_and_status> command_and_status { this, 0x04 };
-            pci_register<reg_type> type { this, 0x08 };
-            pci_register<reg_misc> misc { this, 0x0C };
-            pci_register<std::uintptr_t> base0 { this, 0x10 };
-            pci_register<std::uintptr_t> base1 { this, 0x14 };
-            pci_register<std::uintptr_t> base2 { this, 0x18 };
-            pci_register<std::uintptr_t> base3 { this, 0x1C };
-            pci_register<std::uintptr_t> base4 { this, 0x20 };
-            pci_register<std::uintptr_t> base5 { this, 0x24 };
-            pci_register<std::uintptr_t> cardbus_info { this, 0x28 };
-            pci_register<reg_id> subsystem_id { this, 0x2C };
-            pci_register<std::uintptr_t> expansion_rom_base { this, 0x30 };
-            pci_register<std::uint32_t> capabilities_list { this, 0x34 };
-            pci_register<reg_bus_info> bus_info { this, 0x3C };
-
-            auto read_status() { return command_and_status.read().status; }
-            void clear_status(reg_status clear_bits)
-            {
-                auto s = command_and_status.read();
-                s.status = clear_bits;
-                command_and_status.write(s);
+                if (reg % 4 != 0) throw bad_register { "PCI registers must be aligned to a 32-bit boundary." };
             }
 
-            auto current_command() { return command_and_status.read().command; }
-            void send_command(reg_command cmd)
+            auto read()
             {
-                reg_command_and_status r { };
-                r.command = cmd;
-                command_and_status.write(r);
+                index.write(get_regnum());
+                return data.read();
+            }
+
+            void write(const T& value)
+            {
+                index.write(get_regnum());
+                data.write(value);
             }
 
         private:
-            std::uint16_t index;
-            std::uint8_t bus, device, function;
+            std::uint32_t get_regnum() const
+            {
+                union
+                {
+                    struct
+                    {
+                        unsigned register_num : 8;
+                        unsigned function : 3;
+                        unsigned device : 5;
+                        unsigned bus : 8;
+                        unsigned : 7;
+                        bool enable_config : 1;
+                    };
+                    std::uint32_t value { };
+                } x;
+                x.register_num = reg;
+                x.function = dev.function;
+                x.device = dev.device;
+                x.bus = dev.bus;
+                x.enable_config = true;
+                return x.value;
+            }
 
-            using map_type = std::unordered_map<std::uint16_t, std::unordered_map<std::uint16_t, std::unordered_map<std::uint16_t, const pci_device*>>>;
-            // map indexed by: bus->device->function
-            static map_type* device_map;
-            static void init();
+            const pci_device& dev { };
+            const std::uint8_t reg;
+            static constexpr out_port<std::uint32_t> index { 0xcf8 };
+            static constexpr io_port<T> data { 0xcfc };
         };
-    }
+
+        struct [[gnu::packed]] reg_command
+        {
+            bool io_access : 1;
+            bool memory_access : 1;
+            bool bus_master : 1;
+            bool respond_to_special_cycle : 1;
+            bool enable_memory_write_and_invalidate : 1;
+            bool vga_palette_snoop : 1;
+            bool respond_to_parity_error : 1;
+            bool enable_stepping : 1;               // not used since PCI 3.0
+            bool enable_system_error : 1;
+            bool enable_fast_back_to_back : 1;
+            bool disable_interrupt : 1;
+            unsigned : 5;
+        } command;
+
+        struct [[gnu::packed]] reg_status
+        {
+            unsigned : 3;
+            bool interrupt : 1;
+            bool has_capabilities_list : 1;
+            bool is_66mhz_capable : 1;
+            bool user_definable_configuration : 1;   // not used since PCI 2.2
+            bool is_fast_back_to_back_capable : 1;
+            bool master_parity_error : 1;
+            enum { fast, medium, slow } devsel_timing : 2;
+            bool sent_target_abort : 1;
+            bool received_target_abort : 1;
+            bool received_master_abort : 1;
+            bool sent_system_error : 1;
+            bool parity_error : 1;
+        } status;
+
+        struct [[gnu::packed]] reg_id { std::uint16_t vendor, device; };
+        struct [[gnu::packed]] reg_command_and_status { reg_command command; reg_status status; };
+        struct [[gnu::packed]] reg_type { std::uint8_t revision, prog_interface, subclass, class_code; };
+        struct [[gnu::packed]] reg_bus_info { std::uint8_t irq, interrupt_pin, min_grant, max_latency; };
+        struct [[gnu::packed]] reg_misc
+        {
+            std::uint8_t cache_line_size;
+            std::uint8_t latency_timer;
+            struct [[gnu::packed]]
+            {
+                unsigned header_type : 7;
+                bool multifunction : 1;
+            };
+            struct [[gnu::packed]]
+            {
+                unsigned result : 4;
+                unsigned : 2;
+                bool start : 1;
+                bool is_capable : 1;
+            } self_test;
+        };
+
+        pci_register<reg_id> id { this, 0x00 };
+        pci_register<reg_command_and_status> command_and_status { this, 0x04 };
+        pci_register<reg_type> type { this, 0x08 };
+        pci_register<reg_misc> misc { this, 0x0C };
+        pci_register<std::uintptr_t> base0 { this, 0x10 };
+        pci_register<std::uintptr_t> base1 { this, 0x14 };
+        pci_register<std::uintptr_t> base2 { this, 0x18 };
+        pci_register<std::uintptr_t> base3 { this, 0x1C };
+        pci_register<std::uintptr_t> base4 { this, 0x20 };
+        pci_register<std::uintptr_t> base5 { this, 0x24 };
+        pci_register<std::uintptr_t> cardbus_info { this, 0x28 };
+        pci_register<reg_id> subsystem_id { this, 0x2C };
+        pci_register<std::uintptr_t> expansion_rom_base { this, 0x30 };
+        pci_register<std::uint32_t> capabilities_list { this, 0x34 };
+        pci_register<reg_bus_info> bus_info { this, 0x3C };
+
+        auto read_status() { return command_and_status.read().status; }
+        void clear_status(reg_status clear_bits)
+        {
+            auto s = command_and_status.read();
+            s.status = clear_bits;
+            command_and_status.write(s);
+        }
+
+        auto current_command() { return command_and_status.read().command; }
+        void send_command(reg_command cmd)
+        {
+            reg_command_and_status r { };
+            r.command = cmd;
+            command_and_status.write(r);
+        }
+
+    private:
+        std::uint16_t index;
+        std::uint8_t bus, device, function;
+
+        using map_type = std::unordered_map<std::uint16_t, std::unordered_map<std::uint16_t, std::unordered_map<std::uint16_t, const pci_device*>>>;
+        // map indexed by: bus->device->function
+        static map_type* device_map;
+        static void init();
+    };
 }
--- src/pci.cpp
+++ src/pci.cpp
@@ -1,109 +1,106 @@
-/* * * * * * * * * * * * * * libjwdpmi * * * * * * * * * * * * * */
-/* Copyright (C) 2017 J.W. Jagersma, see COPYING.txt for details */
+#/* * * * * * * * * * * * * * * * * * jwdpmi * * * * * * * * * * * * * * * * * */
+#/*    Copyright (C) 2017 - 2023 J.W. Jagersma, see COPYING.txt for details    */
 
 #include <jw/io/pci.h>
 #include <jw/dpmi/realmode.h>
 
-namespace jw
+namespace jw::io
 {
-    namespace io
+    pci_device::map_type* pci_device::device_map;
+
+    void pci_device::init()
     {
-        pci_device::map_type* pci_device::device_map;
+        dpmi::realmode_registers reg { };
+        reg.ax = 0xb101;
+        reg.edi = 0;
+        reg.call_int(0x1a);
+        if (reg.flags.carry || reg.ah != 0 || reg.edx != 0x20494350) throw unsupported_function { "PCI BIOS not detected." };
+        if (device_map == nullptr) device_map = new map_type { };
+    }
 
-        void pci_device::init()
+    pci_device::pci_device(device_tag, std::uint16_t vendor, std::initializer_list<std::uint16_t> devices, std::uint8_t function_id)
+    {
+        init();
+        for (auto d : devices)
         {
-            dpmi::realmode_registers reg { };
-            reg.ax = 0xb101;
-            reg.edi = 0;
-            reg.call_int(0x1a);
-            if (reg.flags.carry || reg.ah != 0 || reg.edx != 0x20494350) throw unsupported_function { "PCI BIOS not detected." };
-            if (device_map == nullptr) device_map = new map_type { };
-        }
-
-        pci_device::pci_device(device_tag, std::uint16_t vendor, std::initializer_list<std::uint16_t> devices, std::uint8_t function_id)
-        {
-            init();
-            for (auto d : devices)
+            for (auto i = 0;; ++i)
             {
-                for (auto i = 0;; ++i)
-                {
-                    dpmi::realmode_registers reg { };
-                    reg.ax = 0xb102;
-                    reg.cx = d;
-                    reg.dx = vendor;
-                    reg.si = i;
-                    reg.call_int(0x1a);
-                    if (reg.ah == 0x81) throw unsupported_function { "Function \"find PCI device\" not supported." };
-                    if (reg.ah == 0x83) throw device_not_found { "Bad vendor ID." };
-                    if (reg.ah == 0x86) break;
-                    if (reg.flags.carry) throw error { "Unknown PCI BIOS error." };
-                    bus = reg.bh;
-                    device = reg.bl >> 3;
-                    function = reg.bl & 0b111;
-                    if (function_id != 0xff && function != function_id) continue;
-                    if ((*device_map).count(bus) &&
-                        (*device_map)[bus].count(device) &&
-                        (*device_map)[bus][device].count(function)) continue;
-                    (*device_map)[bus][device][function] = this;
-                    return;
-                }
+                dpmi::realmode_registers reg { };
+                reg.ax = 0xb102;
+                reg.cx = d;
+                reg.dx = vendor;
+                reg.si = i;
+                reg.call_int(0x1a);
+                if (reg.ah == 0x81) throw unsupported_function { "Function \"find PCI device\" not supported." };
+                if (reg.ah == 0x83) throw device_not_found { "Bad vendor ID." };
+                if (reg.ah == 0x86) break;
+                if (reg.flags.carry) throw error { "Unknown PCI BIOS error." };
+                bus = reg.bh;
+                device = reg.bl >> 3;
+                function = reg.bl & 0b111;
+                if (function_id != 0xff && function != function_id) continue;
+                if ((*device_map).count(bus) &&
+                    (*device_map)[bus].count(device) &&
+                    (*device_map)[bus][device].count(function)) continue;
+                (*device_map)[bus][device][function] = this;
+                return;
             }
-            throw device_not_found { "PCI Device not found." };
         }
+        throw device_not_found { "PCI Device not found." };
+    }
 
-        pci_device::pci_device(class_tag, std::uint8_t class_code, std::initializer_list<std::uint8_t> subclass_codes, std::uint8_t interface_type)
+    pci_device::pci_device(class_tag, std::uint8_t class_code, std::initializer_list<std::uint8_t> subclass_codes, std::uint8_t interface_type)
+    {
+        init();
+
+        union
         {
-            init();
-
-            union
+            struct [[gnu::packed]]
             {
-                struct [[gnu::packed]]
-                {
-                    unsigned prog_if : 8;
-                    unsigned subclass_c : 8;
-                    unsigned class_c : 8;
-                    unsigned : 8;
-                };
-                const std::uint32_t value { };
-            } ecx;
-            ecx.prog_if = interface_type;
-            ecx.class_c = class_code;
+                unsigned prog_if : 8;
+                unsigned subclass_c : 8;
+                unsigned class_c : 8;
+                unsigned : 8;
+            };
+            const std::uint32_t value { };
+        } ecx;
+        ecx.prog_if = interface_type;
+        ecx.class_c = class_code;
 
-            for (auto c : subclass_codes)
+        for (auto c : subclass_codes)
+        {
+            ecx.subclass_c = c;
+            for (auto i = 0;; ++i)
             {
-                ecx.subclass_c = c;
-                for (auto i = 0;; ++i)
-                {
-                    dpmi::realmode_registers reg { };
-                    reg.ax = 0xb103;
-                    reg.si = i;
-                    reg.ecx = ecx.value;
-                    reg.call_int(0x1a);
-                    if (reg.ah == 0x86) break;
-                    if (reg.flags.carry) throw error { "Unknown PCI BIOS error." };
-                    bus = reg.bh;
-                    device = reg.bl >> 3;
-                    function = reg.bl & 0b111;
-                    if ((*device_map).count(bus) &&
-                        (*device_map)[bus].count(device) &&
-                        (*device_map)[bus][device].count(function)) continue;
-                    (*device_map)[bus][device][function] = this;
-                    return;
-                }
+                dpmi::realmode_registers reg { };
+                reg.ax = 0xb103;
+                reg.si = i;
+                reg.ecx = ecx.value;
+                reg.call_int(0x1a);
+                if (reg.ah == 0x86) break;
+                if (reg.flags.carry) throw error { "Unknown PCI BIOS error." };
+                bus = reg.bh;
+                device = reg.bl >> 3;
+                function = reg.bl & 0b111;
+                if ((*device_map).count(bus) &&
+                    (*device_map)[bus].count(device) &&
+                    (*device_map)[bus][device].count(function)) continue;
+                (*device_map)[bus][device][function] = this;
+                return;
             }
-            throw device_not_found { "PCI Device not found." };
         }
+        throw device_not_found { "PCI Device not found." };
+    }
 
-        pci_device::~pci_device()
+    pci_device::~pci_device()
+    {
+        (*device_map)[bus][device].erase(function);
+        if ((*device_map)[bus][device].empty()) (*device_map)[bus].erase(device);
+        if ((*device_map)[bus].empty()) device_map->erase(bus);
+        if (device_map->empty())
         {
-            (*device_map)[bus][device].erase(function);
-            if ((*device_map)[bus][device].empty()) (*device_map)[bus].erase(device);
-            if ((*device_map)[bus].empty()) device_map->erase(bus);
-            if (device_map->empty())
-            {
-                delete device_map;
-                device_map = nullptr;
-            }
+            delete device_map;
+            device_map = nullptr;
         }
     }
 }
